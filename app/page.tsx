32f7f1e3e8a119c501722cce156b1038a20e36f8
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-29 21:31:03 +0000

    Feat(app): Style home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
-import ClientAuth from "./clientauth";
+import ClientAuth from "./ClientAuth";
+import ListAlbums from "@/components/Music/ListAlbums";
+import Player from "@/components/Music/Player";
 
 export default async function Home() {
   const session = await getServerSession()
-  console.log(session)
+  console.log("Session", session)
   return (
     <>
-      <ClientAuth />
-      <p>Welcome {session?.user?.name} !!! </p>
+      <div className="flex justify-center items-center h-screen flex-col">
+      <div className="flex gap-4">
+        <ClientAuth />
+      </div>
+        <p>Welcome</p>
+      </div>
+      <Player />
     </>
   );
 }
