d7c3ad82e265047c8686f1bcbc0a9ca09a7df85f
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-30 13:57:04 +0000

    Feat(page): Conditional Session Greeting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
-import ClientAuth from "./ClientAuth";
+import ClientAuth from "../components/Authentication/ClientAuth";
 import ListAlbums from "@/components/Music/ListAlbums";
 import Player from "@/components/Music/Player";
 
@@ -8,13 +8,13 @@ export default async function Home() {
   console.log("Session", session)
   return (
     <>
-      <div className="flex justify-center items-center h-screen flex-col">
+      <div className="flex justify-center items-center h-screen flex-col bg-gradient-to-t from-gray-900 to-gray-700">
       <div className="flex gap-4">
         <ClientAuth />
       </div>
-        <p>Welcome</p>
+        {session ? (<p>Welcome {session.user!.username ?? session.user?.name}</p>) : (<p>Hey! Sign in...</p>)}
+        <Player />
       </div>
-      <Player />
     </>
   );
 }
