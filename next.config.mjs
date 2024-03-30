bce6a52e427572f980f4ece062b59bba67616970
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-30 13:56:00 +0000

    Fix(next.config): Allow Amazon Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "m.media-amazon.com",
+      }
+    ]
+  }
+};
 
 export default nextConfig;
