537dda0c293583f82275612081612d329e817c99
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-02 23:57:29 +0100

    Feat(PlusCircle): Add Plus Circle Icon

---
+++ components/Icons/PlusCircle.tsx
@@ -0,0 +1,7 @@
+export default function PlusCircle(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+  )
+}
