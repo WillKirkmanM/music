5d6f072b43bbcbf63294751fc624e4fb0d066a7a
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-04 21:27:45 +0100

    Feat(IconQueue): Add Queue icon for ... queue's

---
+++ components/Icons/IconQueue.tsx
@@ -0,0 +1,7 @@
+export default function IconQueue(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+    </svg>
+  )
+}
