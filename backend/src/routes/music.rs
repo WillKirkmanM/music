194d7d05c361a5e1d9315fa7fc7a10a384f621b2
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-04 18:26:51 +0100

    Feat(music/index_library): Alphabetize and Treat Albums in an array & QOL Improvements

--- backend/src/routes/music.rs
+++ backend/src/routes/music.rs
@@ -1,7 +1,6 @@
 use actix_web::http::header;
 use actix_web::{ HttpResponse, get, Responder, web };
 use tokio::fs;
-use std::os::windows::fs::MetadataExt;
 use std::time::Instant;
 use std::sync::{ Arc, Mutex };
 use std::fs::File;
@@ -10,9 +9,8 @@ use std::io::{Read, SeekFrom, Seek};
 use walkdir::WalkDir;
 use audiotags::Tag;
 use tracing::info;
-use std::collections::HashMap;
 use rayon::prelude::*;
-use crate::structures::structures::Song;
+use crate::structures::structures::{Album, Artist, Song};
 use crate::utils::format::format_contributing_artists;
 
 #[get("/songs/list")]
@@ -42,7 +40,7 @@ async fn index_library(path: web::Path<String>) -> impl Responder {
 
     let now = Instant::now();
 
-    let library = Arc::new(Mutex::new(HashMap::<String, HashMap<String, Vec<Song>>>::new()));
+    let library = Arc::new(Mutex::new(Vec::<Artist>::new()));
 
     let files: Vec<_> = WalkDir::new(&*path)
         .into_iter()
@@ -60,38 +58,63 @@ async fn index_library(path: web::Path<String>) -> impl Responder {
         let artists = tag.artists().unwrap_or_default().iter().map(|&s| s.to_string()).collect::<Vec<String>>();
         let formatted_artists = format_contributing_artists(artists);
 
-        let artist = formatted_artists[0].0.clone();
+        let artist_name = formatted_artists[0].0.clone();
         let contributing_artists = formatted_artists[0].1.clone();
 
-
-        let album = tag.album_title().unwrap_or_default().to_string();
+        let album_name = tag.album_title().unwrap_or_default().to_string();
         let song = Song {
             name: tag.title().unwrap_or_default().to_string(),
-            artist: artist.clone(),
+            artist: artist_name.clone(),
             contributing_artists,
             track_number: tag.track_number().unwrap_or_default(),
             path: path.to_str().unwrap_or_default().to_string(),
         };
-
+    
+    
         let mut library = library.lock().unwrap();
-        library.entry(artist).or_insert_with(HashMap::new).entry(album).or_insert_with(Vec::new).push(song);
+
+        let artist_name_lowercase = artist_name.to_lowercase();
+        let artist_position = library.iter().position(|a| a.name.to_lowercase() == artist_name_lowercase);
+
+        let artist = if let Some(artist_position) = artist_position {
+            &mut library[artist_position]
+        } else {
+            let new_artist = Artist { name: artist_name.clone(), albums: Vec::new() };
+            library.push(new_artist);
+            library.last_mut().unwrap()
+        };
+
+        let album_position = artist.albums.iter().position(|a| a.name == album_name);
+
+        let album = if let Some(album_position) = album_position {
+            &mut artist.albums[album_position]
+        } else {
+            let new_album = Album { name: album_name.clone(), songs: Vec::new() };
+            artist.albums.push(new_album);
+            artist.albums.sort_by(|a, b| a.name.cmp(&b.name));
+            artist.albums.last_mut().unwrap()
+        };
+        
+        album.songs.push(song);
+        album.songs.sort_by(|a, b| a.track_number.cmp(&b.track_number));
     });
 
     let elapsed = now.elapsed().as_millis();
 
     info!("Finished Indexing Library in {}ms", elapsed);
 
-    let library = library.lock().unwrap();
+    let mut library = library.lock().unwrap();
+    library.sort_by(|a, b| a.name.cmp(&b.name));
     let json = serde_json::to_string(&*library).unwrap();
     HttpResponse::Ok().body(json)
 }
 
 #[get("/stream/{song}/{seconds}")]
 async fn stream_song(path: web::Path<(String, u64)>) -> impl Responder {
-    let song = path.0.to_string();
+    // let song = path.0.to_string();
     let seconds = path.1 + 3; // IT offsets by 3 seconds
 
-    // let song = "C:\\Users\\willi\\Documents\\Lawliet\\Music\\Kendrick Lamar - Discography (2009 - 2022) [FLAC] vtwin88cube\\2015 - To Pimp A Butterfly\\12.- Complexion (A Zulu Love).flac";
+    let song = "C:\\Users\\willi\\Documents\\Lawliet\\Music\\Kendrick Lamar - Discography (2009 - 2022) [FLAC] vtwin88cube\\2015 - To Pimp A Butterfly\\12.- Complexion (A Zulu Love).flac";
 
     let file = fs::metadata(song).await.unwrap();
     let song_file_size = file.len();
