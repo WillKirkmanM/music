51c008292b6c56dd5cab26170b43b84f7d758df3
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-21 16:18:28 +0100

    Feat(search): Use seperate imageToBase64 Component

--- apps/web/app/search/page.tsx
+++ apps/web/app/search/page.tsx
@@ -1,4 +1,5 @@
 import BigCard from '@/components/Music/Card/BigCard';
+import imageToBase64 from '@/lib/Image/imageToBase64';
 import { miniSearch } from '@/lib/Search/search';
 import Song from '@/types/Music/Song';
 import fs from "fs"
@@ -36,12 +37,6 @@ export default async function SearchPage({ searchParams }: SearchParamsProps) {
   const query = searchParams.q
 
   const result = minisearch.search(query).slice(0, 20)
-
-  function imageToBase64(src: string) {
-      const image = fs.readFileSync(src);
-      const base64Image = Buffer.from(image).toString('base64');
-      return base64Image;
-  }
 
   return (
     <div className='grid grid-cols-5 gap-4'>
