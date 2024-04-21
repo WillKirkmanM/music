8eb617338a75de08244e170e0c062d8028c495fd
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-21 16:12:04 +0100

    Feat(route.ts): Add search api route

---
+++ apps/web/app/api/search/route.ts
@@ -0,0 +1,13 @@
+import { miniSearch } from "@/lib/Search/search"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const mini = await miniSearch
+
+  const searchParams = req.nextUrl.searchParams
+  const query = searchParams.get("q") ?? ""
+
+  const results = mini.search(query)
+
+  return NextResponse.json({ results })
+}
