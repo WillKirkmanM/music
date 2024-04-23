2bc0635c15c99f229857d3a66d3d003550c36589
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-23 09:44:14 +0100

    Feat(main.rs): Allow docker to run
    Before it was on 127.0.0.1 which only runs on the host machine (in this case the docker container so I can only access it inside the container and not my machine. 0.0.0.0 means all IPv4 addresses on the local machine)

--- apps/web/backend/src/main.rs
+++ apps/web/backend/src/main.rs
@@ -45,7 +45,7 @@ async fn main() -> std::io::Result<()> {
             .service(stream_song)
             .service(format_contributing_artists_route)
     })
-    .bind(("127.0.0.1", port))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
