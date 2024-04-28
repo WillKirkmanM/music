e0a6a27347c3cb1866065fd27b3faf8654677aaa
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-04-28 14:35:39 +0100

    Feat(HomeSelection): Check empty images

--- apps/web/components/Music/HomeSelection.tsx
+++ apps/web/components/Music/HomeSelection.tsx
@@ -1,11 +1,9 @@
-import { Album } from "lucide-react"
-import BigCard from "./Card/BigCard"
-import library from "@/public/music_with_cover_art.json"
-import fs from "fs"
-import type { Library } from "@/types/Music/Library"
+import BigCard from "./Card/BigCard";
+import library from "@/public/music_with_cover_art.json";
+import fs from "fs";
+import type { Library } from "@/types/Music/Library";
 
 export default async function HomeSelection() {
-
   // const json = await fetch("http:/localhost:3001/music.json")
   // let library: Library = await json.json()
 
@@ -16,48 +14,55 @@ export default async function HomeSelection() {
   //   </div>
   // ));
 
+  let typedLibrary: Library = library;
+  const allSongs = typedLibrary.flatMap((artist) =>
+    artist.albums.flatMap((album) =>
+      album.songs.map((song) => ({
+        ...song,
+        artist: artist.name,
+        album: album.name,
+        image: album.cover_url,
+      }))
+    )
+  );
 
-  let typedLibrary: Library = library
-  // <BigCard title="You Da One" artistName="Rihanna" imageSrc="https://m.media-amazon.com/images/I/51BpyM50BhL._UXNaN_FMjpg_QL85_.jpg" albumURL="" songURL=""/>
-const allSongs = typedLibrary.flatMap(artist => 
-  artist.albums.flatMap(album => 
-    album.songs.map(song => ({ ...song, artist: artist.name, album: album.name, image: album.cover_url }))
-  )
-);
+  // Shuffle the array
+  for (let i = allSongs.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allSongs[i], allSongs[j]] = [allSongs[j], allSongs[i]];
+  }
 
-// Shuffle the array
-for (let i = allSongs.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [allSongs[i], allSongs[j]] = [allSongs[j], allSongs[i]];
-}
+  const randomSongs = allSongs.slice(0, 5);
+  // randomSongs.forEach(song => console.log(`http://localhost:3001/stream/${encodeURIComponent(song.path)}`))
+  // randomSongs.forEach(song => console.log(song.image))
 
-const randomSongs = allSongs.slice(0, 5);
-// randomSongs.forEach(song => console.log(`http://localhost:3001/stream/${encodeURIComponent(song.path)}`))
-// randomSongs.forEach(song => console.log(song.image))
-
-function imageToBase64(src: string) {
+  function imageToBase64(src: string) {
     const image = fs.readFileSync(src);
-    const base64Image = Buffer.from(image).toString('base64');
+    const base64Image = Buffer.from(image).toString("base64");
     return base64Image;
-}
+  }
 
-return (
-  <>
-    <div className="flex flex-row px-5 ">
-    {randomSongs.map((song, index) => (
-      <div className="mr-20" key={index}>
-        <BigCard
-          title={song.name}
-          artistName={song.artist}
-          imageSrc={song.image.startsWith("http") ? "/snf.png" : `data:image/jpg;base64,${imageToBase64(song.image)}`}
-          albumURL=""
-          songURL={`http://localhost:3001/stream/${encodeURIComponent(song.path)}`}
-          type="Song"
-          song={song}
-          />
+  return (
+    <>
+      <div className="flex flex-row px-5 ">
+        {randomSongs.map((song, index) => (
+          <div className="mr-20" key={index}>
+            <BigCard
+              title={song.name}
+              artistName={song.artist}
+              imageSrc={
+                song.image.length === 0
+                  ? "/snf.png"
+                  : `data:image/jpg;base64,${imageToBase64(song.image)}`
+              }
+              albumURL=""
+              songURL={`http://localhost:3001/stream/${encodeURIComponent(song.path)}`}
+              type="Song"
+              song={song}
+            />
+          </div>
+        ))}
       </div>
-    ))}
-    </div>
-  </>
-);
+    </>
+  );
 }
