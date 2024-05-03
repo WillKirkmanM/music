3423ab230a8bc76332c8bd7c97d5f1801b8be98d
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-03 11:05:36 +0100

    Feat(Navbar): Add ToggleTheme button to navbar

--- apps/web/components/Layout/Navbar.tsx
+++ apps/web/components/Layout/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import ClientAuth from "../Authentication/ClientAuth";
 import SearchBar from "../Search/SearchBar";
+import ToggleTheme from "../Themes/ToggleTheme";
 
 export default function NavBar() {
   return (
     <>
-      <nav className="fixed w-full top-0 p-4 flex items-center justify-between">
+      <nav className="fixed w-full top-0 p-4 flex items-center justify-between gap-2 border-b border-gray-300">
         <Link href="/">
           <div className="text-2xl text-white font-bold hover:text-gray-300">
             ParsonLabs Music
@@ -14,9 +15,11 @@ export default function NavBar() {
 
         <SearchBar />
 
-        <ClientAuth />
+        <div className="flex gap-3">
+          <ClientAuth />
+          <ToggleTheme />
+        </div>
       </nav>
-    {/* <div className="pt-16"/> */}
     </> 
   )
 }
