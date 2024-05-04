3dff8ddc87822237ade77b9ae759fb6f5c40f85f
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-04 22:56:52 +0100

    Feat(Sidebar): Add inset and icons to playlists

--- apps/web/components/Layout/Sidebar.tsx
+++ apps/web/components/Layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import prisma from "@/prisma/prisma";
 import { Button } from "@music/ui/components/button";
 import CreatePlaylistDialog from "../Music/Playlist/CreatePlaylistDialog";
-import { Plus } from "lucide-react";
+import { ListMusic, Plus } from "lucide-react";
 
 export async function Sidebar() {
   const user = await getServerSession()
@@ -11,15 +11,16 @@ export async function Sidebar() {
   if (user) playlists = await prisma.playlist.findMany({ where: { users: { some: { username: user.user.username } } } })
   return (
     <aside className="fixed left-0 hidden h-full w-1/5 space-y-2 p-4 lg:block xl:w-[15%] border-r">
-      <h3 className="font-heading text-xl">
-        Welcome
-      </h3>
+      <h3 className="font-heading text-xl">Welcome</h3>
 
       {playlists?.map(playlist => {
         return (
-          <Link href={"/playlist/" + playlist.id} key={playlist.id}>
-            <p key={playlist.id}>{playlist.name}</p>
-          </Link>
+          <div className="pl-5 text-sm flex flex-row items-center gap-3 py-2" key={playlist.id}>
+            <ListMusic className="size-4"/>
+            <Link href={"/playlist/" + playlist.id} key={playlist.id}>
+              <p key={playlist.id}>{playlist.name}</p>
+            </Link>
+          </div>
         )
       })}
  
