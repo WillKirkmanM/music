17db1f18df96c18d4334a8a947a070561e68f3f1
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-04 16:33:21 +0100

    Feat(Sidebar): Add Playlists & Create playlist button

--- apps/web/components/Layout/Sidebar.tsx
+++ apps/web/components/Layout/Sidebar.tsx
@@ -1,10 +1,14 @@
 import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
 import Link from "next/link";
+import prisma from "@/prisma/prisma";
+import { Button } from "@music/ui/components/button";
+import IconPlus from "../Icons/IconPlus";
+import CreatePlaylistDialog from "../Music/Playlist/CreatePlaylistDialog";
 
 export async function Sidebar() {
   const user = await getServerSession()
   let playlists;
-  if (user) playlists = await prisma?.playlist.findMany({ where: { users: { some: { username: user.user.username } } } })
+  if (user) playlists = await prisma.playlist.findMany({ where: { users: { some: { username: user.user.username } } } })
   return (
     <aside className="fixed left-0 hidden h-full w-1/5 space-y-2 p-4 lg:block xl:w-[15%] border-r">
       <h3 className="font-heading text-xl">
@@ -14,10 +18,17 @@ export async function Sidebar() {
       {playlists?.map(playlist => {
         return (
           <Link href={"/playlist/" + playlist.id} key={playlist.id}>
-            <p key={playlist.id}>{playlist.name} {playlist.id}</p>
+            <p key={playlist.id}>{playlist.name}</p>
           </Link>
         )
       })}
+ 
+      <CreatePlaylistDialog username={user!.user.username}>
+        <Button>
+          <IconPlus />
+          Create Playlist
+        </Button>
+      </CreatePlaylistDialog>
 
     </aside>
   );
