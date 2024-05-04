f3246816a51b6a05aebe0a0133675e7221e5583d
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-04 18:10:57 +0100

    Feat(PlaylistTable): Fix Underflowing Songs

--- apps/web/components/Music/Playlist/PlaylistTable.tsx
+++ apps/web/components/Music/Playlist/PlaylistTable.tsx
@@ -42,30 +42,32 @@ export default function PlaylistTable({ songsWithMetadata }: PlaylistTableProps)
   }
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[80px]">#</TableHead>
-          <TableHead className="w-[200px]">Title</TableHead>
-          <TableHead className="w-[200px]">Album</TableHead>
-          <TableHead className="text-right">Artist</TableHead>
-        </TableRow>
-      </TableHeader>
-
-      {songsWithMetadata.map((song, index) => (
-        <TableBody key={song.song.id}>
-          <TableRow onClick={() => handlePlay(song.coverURL, song.song, `http://localhost:3001/stream/${encodeURIComponent(song.path)}`)}>
-            <TableCell className="font-medium">{index + 1}</TableCell>
-            <TableCell>
-              <div className="w-[300px] overflow-hidden whitespace-nowrap text-overflow">
-                <PlaylistCard song={song.song} coverURL={song.coverURL} />
-              </div>
-            </TableCell>
-            <TableCell>{song.albumName}</TableCell>
-            <TableCell className="text-right">{song.artistName}</TableCell>
+    <div className="pb-36">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[80px]">#</TableHead>
+            <TableHead className="w-[200px]">Title</TableHead>
+            <TableHead className="w-[200px]">Album</TableHead>
+            <TableHead className="text-right">Artist</TableHead>
           </TableRow>
-        </TableBody>
-      ))}
-    </Table>
+        </TableHeader>
+
+        {songsWithMetadata.map((song, index) => (
+          <TableBody key={song.song.id}>
+            <TableRow onClick={() => handlePlay(song.coverURL, song.song, `http://localhost:3001/stream/${encodeURIComponent(song.path)}`)}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell>
+                <div className="w-[300px] overflow-hidden whitespace-nowrap text-overflow">
+                  <PlaylistCard song={song.song} coverURL={song.coverURL} />
+                </div>
+              </TableCell>
+              <TableCell>{song.albumName}</TableCell>
+              <TableCell className="text-right">{song.artistName}</TableCell>
+            </TableRow>
+          </TableBody>
+        ))}
+      </Table>
+    </div>
   )
 }
