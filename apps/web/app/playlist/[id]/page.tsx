243d529024549f33e5a78e535bfd715f47e0210a
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-04 22:56:33 +0100

    Feat(Playlist): Add Delete Playlist Button

--- apps/web/app/playlist/[id]/page.tsx
+++ apps/web/app/playlist/[id]/page.tsx
@@ -1,16 +1,12 @@
 import prisma from "@/prisma/prisma";
 import { Playlist, Song as SSong, User } from "@prisma/client";
-import { redirect } from "next/navigation";
-import Image from "next/image";
 import art from "@/public/music_with_cover_art.json";
 import Artist from "@/types/Music/Artist";
 import Song from "@/types/Music/Song";
 import Album from "@/types/Music/Album";
-import BigCard from "@/components/Music/Card/BigCard";
-import imageToBase64 from "@/lib/Image/imageToBase64";
 import PlaylistTable from "@/components/Music/Playlist/PlaylistTable";
-import { Avatar, AvatarFallback, AvatarImage } from "@music/ui/components/avatar";
-import { Users } from "lucide-react";
+import { Avatar, AvatarFallback } from "@music/ui/components/avatar";
+import DeletePlaylistButton from "@/components/Music/Playlist/DeletePlaylistButton";
 
 type PlaylistPageParams = {
   params: {
@@ -53,20 +49,25 @@ export default async function PlaylistPage({ params }: PlaylistPageParams) {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-center space-x-5 m-10">
-        <div>
-          <Avatar className="w-24 h-24 text-4xl">
-            <AvatarFallback>{playlist.users[0]?.username?.substring(0, 2).toUpperCase()}</AvatarFallback>
-          </Avatar>
+      <div className="flex flex-row items-start justify-between space-x-5 m-10">
+        <div className="flex items-center justify-center space-x-5 flex-grow">
+          <div>
+            <Avatar className="w-24 h-24 text-4xl">
+              <AvatarFallback>{playlist.users[0]?.username?.substring(0, 2).toUpperCase()}</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="text-center">
+            <h1 className="text-3xl">{playlist.name}</h1>
+            {playlist.users.map(user => {
+              return <h1 key={user.id} className="text-2xl">Created by {user.username}</h1>
+            })}
+          </div>
         </div>
-        <div className="text-center">
-          <h1 className="text-3xl">{playlist.name}</h1>
-          {playlist.users.map(user => {
-            return <h1 key={user.id} className="text-2xl">Created by {user.username}</h1>
-          })}
+        <div className="flex items-start">
+          <DeletePlaylistButton playlistID={params.id} />
         </div>
       </div>
       <PlaylistTable songsWithMetadata={songsWithMetadata} />
     </>
-);
+  );
 }
