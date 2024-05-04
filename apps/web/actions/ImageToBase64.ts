121c395c8e14b250a9caef5270ced88553252e9c
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-04 16:31:10 +0100

    Feat(ImageToBase64): Move I2B64 to Server Action

---
+++ apps/web/actions/ImageToBase64.ts
@@ -0,0 +1,9 @@
+"use server"
+
+import fs from "fs"
+
+export default async function imageToBase64(src: string) {
+  const image = fs.readFileSync(src);
+  const base64Image = Buffer.from(image).toString('base64');
+  return base64Image;
+}
