11eadd8cb9432abaa65a1d08e982816247e22d9a
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-12 00:15:54 +0100

    RF(app): Remove Imports

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,4 @@
-import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
-import ClientAuth from "../components/Authentication/ClientAuth";
-import ListAlbums from "@/components/Music/ListAlbums";
-import Player from "@/components/Music/Player";
 import HomeSelection from "@/components/Music/HomeSelection";
-import { PlayerProvider } from "@/components/Music/Player/usePlayer";
-import { Suspense } from "react";
-import ToggleTheme from "@/components/Themes/ToggleTheme";
 
 export default async function Home() {
   return (
