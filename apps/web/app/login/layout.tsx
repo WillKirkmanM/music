9ef08fc5f7d3017e9206fd6e9e3ec29a6e33aeef
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-05-20 23:52:10 +0100

    Feat(login): Layout Page

---
+++ apps/web/app/login/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "ParsonLabs Music Login Page"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
