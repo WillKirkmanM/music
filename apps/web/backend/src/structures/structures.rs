6f53c321584562626a211bbbbf8da2b5b45f5f9e
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-02 22:41:46 +0100

    Feat(structures.rs): Add duration and icon url for artists

--- apps/web/backend/src/structures/structures.rs
+++ apps/web/backend/src/structures/structures.rs
@@ -1,13 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct Artist {
-    pub id: u64,
-    pub name: String,
-    pub albums: Vec<Album>,
-}
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Album {
     pub id: u64,
     pub name: String,
@@ -15,7 +8,16 @@ pub struct Album {
     pub songs: Vec<Song>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Artist {
+    pub id: u64,
+    pub name: String,
+    pub icon_url: String,
+    pub followers: u64,
+    pub albums: Vec<Album>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Song {
     pub id: u64,
     pub name: String,
@@ -23,4 +25,5 @@ pub struct Song {
     pub contributing_artists: Vec<String>,
     pub track_number: u16,
     pub path: String,
+    pub duration: f64,
 }
