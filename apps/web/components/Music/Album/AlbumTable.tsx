3ee68a3ecf60d48c31260a5f0d5d29acc5cb0768
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-02 22:47:51 +0100

    Feat(AlbumTable): Add duration

--- apps/web/components/Music/Album/AlbumTable.tsx
+++ apps/web/components/Music/Album/AlbumTable.tsx
@@ -9,6 +9,8 @@ import {
   TableHeader,
   TableRow,
 } from "@music/ui/components/table"
+import {} from "@tanstack/react-table"
+
 import { usePlayer } from "@/components/Music/Player/usePlayer";
 import Song from "@/types/Music/Song";
 import imageToBase64 from "@/actions/ImageToBase64";
@@ -16,6 +18,8 @@ import SongContextMenu from "../SongContextMenu";
 import Album from "@/types/Music/Album";
 import Artist from "@/types/Music/Artist";
 import Link from "next/link";
+import { startTransition, useEffect, useState } from "react";
+import getServerIpAddress from "@/actions/System/GetIpAddress";
 
 type PlaylistTableProps = {
   songs: Song[]
@@ -25,6 +29,16 @@ type PlaylistTableProps = {
 
 export default function AlbumTable({ songs, album, artist }: PlaylistTableProps) {
   const { setImageSrc, setSong, setAudioSource, setArtist, setAlbum } = usePlayer()
+  const [serverIP, setServerIP] = useState("");
+
+  useEffect(() => {
+    async function getServerIP() {
+      const ip = await getServerIpAddress()
+      setServerIP(ip)
+    }
+
+    getServerIP()
+  })
 
   const handlePlay = async (coverURL: string, song: Song, songURL: string, artist: Artist) => {
     let base64Image = coverURL
@@ -39,6 +53,12 @@ export default function AlbumTable({ songs, album, artist }: PlaylistTableProps)
     setAudioSource(songURL)
   }
 
+  function formatDuration(duration: number) {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.round(duration % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
   return (
     <div className="pb-36">
       <Table>
@@ -46,23 +66,21 @@ export default function AlbumTable({ songs, album, artist }: PlaylistTableProps)
           <TableRow>
             <TableHead className="w-[80px]">#</TableHead>
             <TableHead className="w-[200px]">Title</TableHead>
-            <TableHead className="w-[200px]">Album</TableHead>
-            <TableHead className="text-right">Artist</TableHead>
+            <TableHead className="text-right">Duration</TableHead>
           </TableRow>
         </TableHeader>
 
         {songs.map(song => (
           <SongContextMenu song={song} album={album} artist={artist} key={song.id}>
             <TableBody key={song.id}>
-              <TableRow onClick={() => handlePlay(album.cover_url, song, `http://localhost:3001/stream/${encodeURIComponent(song.path)}`, artist)}>
+              <TableRow onClick={() => handlePlay(album.cover_url, song, `http://${serverIP}:3001/stream/${encodeURIComponent(song.path)}`, artist)}>
                 <TableCell className="font-medium">{song.track_number}</TableCell>
                 <TableCell>
                   <div className="w-[300px] overflow-hidden whitespace-nowrap text-overflow">
                     <PlaylistCard song={song} coverURL={album.cover_url} artist={artist} album={album} />
                   </div>
                 </TableCell>
-                <TableCell><Link href={`/album/${album.id}`}>{album.name}</Link></TableCell>
-                <TableCell className="text-right"><Link href={`/album/${artist.id}`}>{artist.name}</Link></TableCell>
+                <TableCell className="text-right">{formatDuration(song.duration)}</TableCell>
               </TableRow>
             </TableBody>
           </SongContextMenu>
