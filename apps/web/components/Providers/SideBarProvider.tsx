5775dcb56f94ce2537e7215f3aaa169761f69001
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-02 22:54:20 +0100

    Feat(SideBarProvider): Sidebar Context to see if closed

---
+++ apps/web/components/Providers/SideBarProvider.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { createContext, useState, useContext, ReactNode } from 'react';
+
+type SidebarContextType = {
+  isOpen: boolean;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+  toggleSidebar: () => void;
+};
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+type SidebarProviderProps = {
+  children: ReactNode
+}
+
+export default function SidebarProvider({ children }: SidebarProviderProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const openSidebar = () => setIsOpen(true);
+  const closeSidebar = () => setIsOpen(false);
+  const toggleSidebar = () => setIsOpen(prevIsOpen => !prevIsOpen);
+
+  return (
+    <SidebarContext.Provider value={{ isOpen, openSidebar, closeSidebar, toggleSidebar }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (context === undefined) {
+    throw new Error('useSidebar must be used within a SidebarProvider');
+  }
+  return context;
+};
