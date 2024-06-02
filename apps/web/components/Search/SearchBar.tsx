64171fd0fa85c7d3af92900545db0b280148536d
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-02 22:54:34 +0100

    Feat(SearchBar): Responsive on Mobile

--- apps/web/components/Search/SearchBar.tsx
+++ apps/web/components/Search/SearchBar.tsx
@@ -1,26 +1,42 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Input } from "@music/ui/components/input"
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+import { Input } from "@music/ui/components/input";
+import { Search } from "lucide-react";
 
-export default function SearchBar() {
+type SearchBarProps = {
+  isSearchActive: boolean,
+  setIsSearchActive: Function
+}
+
+export default function SearchBar({ isSearchActive, setIsSearchActive }: SearchBarProps) {
   const [query, setQuery] = useState("");
   const router = useRouter();
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     router.push(`/search?q=${query}`);
+    setIsSearchActive(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input 
-        className="w-96" 
-        placeholder="In Da Club..." 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)}
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className={`${isSearchActive ? "w-full flex" : "hidden"} md:flex md:w-96`}
+      >
+        <Input
+          className="flex-1 text-black"
+          placeholder="In Da Club..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
+      <Search
+        className="md:hidden cursor-pointer"
+        onClick={() => setIsSearchActive(!isSearchActive)}
       />
-    </form>
-  )
+    </>
+  );
 }
