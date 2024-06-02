deb702e0185f899b1c20ca3291caf04db88a0ea3
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-02 22:47:25 +0100

    Feat(Player): Responsivity

--- apps/web/components/Music/Player.tsx
+++ apps/web/components/Music/Player.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { useState, useRef, useContext } from "react";
+import { useState, useRef, useContext, useEffect } from "react";
 import { PanelContext } from "./Queue/QueuePanelContext";
 import IconPause from "../Icons/Pause";
 import IconPlay from "../Icons/Play";
@@ -11,9 +11,10 @@ import CheckCircle from "../Icons/CheckCircle";
 import ArrowPath from "../Icons/ArrowPath";
 import SpeakerXMark from "../Icons/SpeakerXMark";
 import SpeakerWave from "../Icons/SpeakerWave";
-import { MicVocal } from "lucide-react";
+import { MicVocal, SkipBack, SkipForward } from "lucide-react";
 import IconQueue from "../Icons/IconQueue";
 import { usePlayer } from "./Player/usePlayer";
+import Head, { defaultHead } from "next/head"
 import AddToPlaylistDropdown from "./Player/AddToPlaylistDropdown";
 import {
   Slider,
@@ -23,6 +24,8 @@ import {
 } from "@music/ui/components/slider";
 import Link from "next/link";
 import { LyricsContext } from "../Lyrics/LyricsOverlayContext";
+import { useGradientHover } from "../Providers/GradientHoverProvider";
+import { FastAverageColor } from "fast-average-color";
 
 export default function Player() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
@@ -46,6 +49,8 @@ export default function Player() {
     handleTimeUpdate,
     toggleMute,
     imageSrc,
+    playNextSong,
+    playPreviousSong,
     song,
     bufferedTime,
     artist,
@@ -58,8 +63,34 @@ export default function Player() {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
-  return (
-    <footer className="fixed bottom-0 bg-gray-600 border-t border-gray-700 px-3 py-3 flex flex-col md:flex-row items-center justify-between md:gap-4 w-full">
+  useEffect(() => {
+    if (isPlaying) document.title = `${song.name} | ParsonLabs Music`;
+    if (!isPlaying) document.title = `ParsonLabs Music`
+  }, [song, artist, isPlaying]);
+
+  const { gradient } = useGradientHover()
+
+  const [backgroundColour, setBackgroundColour] = useState("")
+
+  useEffect(() => {
+    if(imageSrc) {
+      const fac = new FastAverageColor();
+      const getColor = async () => {
+        const color = await fac.getColorAsync(imageSrc);
+        setBackgroundColour(color.hex);
+      };
+      getColor();
+    }
+  }, [imageSrc]);
+
+  return song.id && (
+    // <footer className="z-50 fixed bottom-0 border-t bg-black border-gray-800 px-3 py-3 flex flex-col md:flex-row items-center justify-between md:gap-4 w-full">
+  <footer className="z-50 fixed bottom-0 border-t border-gray-800 px-3 py-3 flex flex-col md:flex-row items-center justify-between md:gap-4 w-full" 
+    style={{ 
+      backgroundColor: "#212121"
+      //  backgroundImage: `linear-gradient(90deg, ${backgroundColour}, ${backgroundColour})`,
+      //  backdropFilter: 'blur(100px)'
+    }}>
       <section className="flex flex-col md:flex-row items-center justify-between gap-3 w-full md:w-1/4">
         <div className="flex items-center w-full md:w-1/2 justify-center">
           <Slider
@@ -69,7 +100,7 @@ export default function Player() {
             onValueChange={([values]) => {
               handleTimeChange(Number(values));
             }}
-            className="w-full group"
+            className="w-full group md:hidden"
           >
             <SliderTrack className="h-1 bg-gray-400 cursor-pointer">
               <SliderRange
@@ -80,14 +111,6 @@ export default function Player() {
             </SliderTrack>
             <SliderThumb className="cursor-pointer bg-white hidden group-hover:block size-4" />
           </Slider>
-          <div className="hidden md:flex items-center gap-2">
-            <span className="text-xs text-gray-400">
-              {formatTime(currentTime)}
-            </span>
-            <span className="text-xs text-gray-400">
-              {formatTime(duration)}
-            </span>
-          </div>
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-3">
@@ -118,9 +141,43 @@ export default function Player() {
           </div>
         </div>
       </section>
+
+      <section className="md:flex flex-col items-center gap-2 w-full hidden">
+        <div className="flex items-center gap-4">
+          <button onClick={() => playPreviousSong()}><SkipBack className="w-5 h-5" /></button>
+          <button onClick={() => togglePlayPause()}>
+            {isPlaying ? <IconPause /> : <IconPlay />}
+          </button>
+          <button onClick={() => playNextSong()}><SkipForward className="w-5 h-5"/></button>
+          <button onClick={() => toggleLoopSong}>
+            {onLoop ? <ArrowPath style={{ strokeWidth: 2, color: 'white' }} /> : <ArrowPath />}
+          </button>
+        </div>
+        <div className="flex items-center gap-2 w-full justify-center">
+          <span className="text-xs text-gray-400">{formatTime(currentTime)}</span>
+
+        <Slider
+          min={0}
+          max={duration}
+          value={[currentTime]}
+          onValueChange={([values]) => {
+            handleTimeChange(Number(values))
+          }}
+          className="w-1/2 group"
+        >
+          <SliderTrack className="h-1 bg-gray-400 cursor-pointer">
+            <SliderRange className="bg-gray-500" style={{ width: `${(bufferedTime / duration) * 100}%` }} />
+            <SliderRange className="bg-black" />
+          </SliderTrack>
+
+          <SliderThumb className="cursor-pointer bg-white hidden group-hover:block size-4" />
+        </Slider>
+            <span className="text-xs text-gray-400">{formatTime(duration)}</span>
+        </div>
+      </section>
+
+
       <section className="hidden md:flex items-center gap-2">
-        <button>List</button>
-        <button>Devices</button>
         <button onClick={toggleLyrics}>
           <MicVocal />
         </button>
@@ -146,7 +203,6 @@ export default function Player() {
             <SliderThumb className="cursor-pointer bg-white hidden group-hover:block size-4" />
           </Slider>
         </div>
-        <button>Fullscreen</button>
       </section>
       <audio ref={audioRef} onTimeUpdate={() => handleTimeUpdate} />
     </footer>
