1a523029a461a8dd1c0e67c0e9dfffe34539dcc2
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-09 12:29:45 +0100

    Feat(Album.ts): Add Description, release date to type

--- apps/web/types/Music/Album.ts
+++ apps/web/types/Music/Album.ts
@@ -4,5 +4,10 @@ export default interface Album {
   id: number
   name: string
   cover_url: string
+  description: string
   songs: Song[]
+  first_release_date: string
+  musicbrainz_id: string
+  wikidata_id: string | null
+  primary_type: string
 }
