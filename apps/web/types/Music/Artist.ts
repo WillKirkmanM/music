be57511f7c4e7d498bb88004158dfa2322a19883
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-09 12:29:20 +0100

    Feat(Artist): Add Description to Type

--- apps/web/types/Music/Artist.ts
+++ apps/web/types/Music/Artist.ts
@@ -3,8 +3,8 @@ import type Album from "./Album"
 export default interface Artist {
   id: number
   name: string
-  icon_url: string,
+  icon_url: string
   followers: number
   albums: Album[]
+  description: string
 }
-
