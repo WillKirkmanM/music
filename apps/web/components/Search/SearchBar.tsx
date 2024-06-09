e6a8196c1905e9ff8283ea3cccc434a39bfb1a87
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-09 23:59:35 +0100

    Feat(SearchBar): Gray SearchBar Colour

--- apps/web/components/Search/SearchBar.tsx
+++ apps/web/components/Search/SearchBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useContext } from "react";
 import { useRouter } from "next/navigation";
 import { Input } from "@music/ui/components/input";
 import { Search } from "lucide-react";
+import { ScrollContext } from "../Providers/ScrollProvider";
 
 type SearchBarProps = {
   isSearchActive: boolean,
@@ -12,6 +13,7 @@ type SearchBarProps = {
 
 export default function SearchBar({ isSearchActive, setIsSearchActive }: SearchBarProps) {
   const [query, setQuery] = useState("");
+  const { onTopOfPage } = useContext(ScrollContext)
   const router = useRouter();
 
   const handleSubmit = (event: FormEvent) => {
@@ -26,12 +28,12 @@ export default function SearchBar({ isSearchActive, setIsSearchActive }: SearchB
         onSubmit={handleSubmit}
         className={`${isSearchActive ? "w-full flex" : "hidden"} md:flex md:w-96`}
       >
-        <Input
-          className="flex-1 text-black"
-          placeholder="In Da Club..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+      <Input
+        className={`flex-1 text-white border border-[#6e777d] ${onTopOfPage ? "opacity-60 bg-gradient-to-r from-[#2f353a80] to-[#34373580]" : "bg-white"}`}
+        placeholder="In Da Club..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       </form>
       <Search
         className="md:hidden cursor-pointer"
