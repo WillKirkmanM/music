656abfde6cb678830a6bbbe0c3358f9f284ae1a2
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-06-14 21:30:41 +0100

    Feat(GetIpAddress): First check ENV variable (HOST_IP) then check wifi and then NIC

--- apps/web/actions/System/GetIpAddress.ts
+++ apps/web/actions/System/GetIpAddress.ts
@@ -3,10 +3,25 @@
 import os from 'os';
 
 export default async function getServerIpAddress() {
+  if (process.env.HOST_IP) {
+    return process.env.HOST_IP;
+  }
+
   const networkInterfaces = os.networkInterfaces();
 
   if (networkInterfaces['Wi-Fi']) {
     for (const net of networkInterfaces['Wi-Fi']) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+
+  for (const interfaceName in networkInterfaces) {
+    if (interfaceName === 'Wi-Fi') continue;
+
+    const netInterface = networkInterfaces[interfaceName] as os.NetworkInterfaceInfo[];
+    for (const net of netInterface) {
       if (net.family === 'IPv4' && !net.internal) {
         return net.address;
       }
