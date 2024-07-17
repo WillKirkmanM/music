f8a6dcd2bf2aa0d8162a74c66a26dcc1c89840b6
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-07-17 22:23:11 +0100

    Fix(main.rs): Compare Returning Empty Metadata

--- apps/web/backend/src/routes/music.rs
+++ apps/web/backend/src/routes/music.rs
@@ -1,39 +1,41 @@
 use actix_web::http::header;
-use actix_web::{ get, web, HttpRequest, HttpResponse, Responder };
+use actix_web::{get, web, HttpRequest, HttpResponse, Responder};
+use futures::stream::StreamExt;
 use lofty::{AudioFile, TaggedFile};
 use serde::Deserialize;
-use tokio::fs;
-use tokio::process::Command;
-use tokio_util::io::ReaderStream;
 use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
 use std::path::Path;
 use std::process::Stdio;
 use std::time::Instant;
-use std::fs::File;
-use futures::stream::StreamExt;
-use std::io::{Read, Seek, SeekFrom};
+use tokio::fs;
+use tokio::process::Command;
+use tokio_util::io::ReaderStream;
 
-use walkdir::WalkDir;
-use tracing::{info, warn};
+use crate::structures::structures::Artist;
 use crate::utils::compare::compare;
-use crate::utils::config::save_config;
-use crate::utils::metadata::{process_album, process_albums, process_artist, process_artists, get_access_token};
-use crate::utils::websocket::log_to_ws;
+use crate::utils::config::{get_config, save_config};
 use crate::utils::format::format_contributing_artists;
 use crate::utils::library::index_library;
+use crate::utils::metadata::{
+    get_access_token, process_album, process_albums, process_artist, process_artists,
+};
+use crate::utils::websocket::log_to_ws;
+use tracing::{info, warn};
+use walkdir::WalkDir;
 
-#[get("/songs/list")]
-pub async fn songs_list() -> impl Responder {
-
+#[get("/songs/list/{path}")]
+pub async fn songs_list(path: web::Path<String>) -> impl Responder {
     let now = Instant::now();
 
     let mut i = 0;
-    for entry in WalkDir::new("C:\\Users\\willi\\Documents\\Lawliet\\Music") {
+    for entry in WalkDir::new(path.as_str()) {
         let entry = entry.unwrap();
         let extension = entry.path().extension().unwrap_or_default();
 
         if entry.file_type().is_file() && extension == "mp3" || extension == "flac" {
-            i+=1;
+            i += 1;
             // println!("{}", entry.path().display());
         }
     }
@@ -44,63 +46,91 @@ pub async fn songs_list() -> impl Responder {
 }
 
 #[get("/library/index/{pathToLibrary}")]
-async fn process_library(path_to_library: web::Path<String>, ) -> impl Responder {
+async fn process_library(path_to_library: web::Path<String>) -> impl Responder {
     info!("Indexing library...");
     log_to_ws("Indexing library...".to_string()).await.unwrap();
 
     let now = Instant::now();
-
     let library = index_library(path_to_library.as_str()).await.unwrap();
 
     let client = reqwest::Client::builder()
-            .user_agent("ParsonLabsMusic/0.1 (will@parsonlabs.com)")
-            .build()
-            .unwrap();
+        .user_agent("ParsonLabsMusic/0.1 (will@parsonlabs.com)")
+        .build()
+        .unwrap();
 
-        match compare(&library).await {
-            Ok((mut new_artist_entries, new_album_entries, _new_song_entries)) => {
-                if !new_artist_entries.is_empty() {
-                    let artists_without_icon_count = new_artist_entries.iter()
-                        .filter(|artist| artist.icon_url.is_empty())
-                        .count();
-                    let log = format!("Searching icon art for {} artists...", artists_without_icon_count);
-                    info!(log);
-                    log_to_ws(log).await.unwrap();
-                    
-                    match get_access_token().await {
-                        Ok(access_token) => {
-                            for artist in new_artist_entries.iter_mut() {
-                                process_artist(&client, artist, access_token.clone()).await;
-                            }
-                        },
-                        Err(e) => warn!("Failed to get access token. Error: {}", e),
+    let mut current_library: Vec<Artist> =
+        serde_json::from_str(get_config().await.unwrap_or("".to_string()).as_str()).unwrap();
+
+    if current_library.is_empty() {
+        let mut library_guard = library.lock().unwrap();
+        process_artists(&client, &mut *library_guard).await;
+        process_albums(&client, &mut *library_guard).await;
+    }
+
+    if let Ok((mut new_artist_entries, mut new_album_entries, _new_song_entries)) =
+        compare(&library).await
+    {
+        if !new_artist_entries.is_empty() {
+            let artists_without_icon_count = new_artist_entries
+                .iter()
+                .filter(|artist| artist.icon_url.is_empty())
+                .count();
+            let log = format!(
+                "Searching icon art for {} artists...",
+                artists_without_icon_count
+            );
+            info!(log);
+            log_to_ws(log).await.unwrap();
+
+            match get_access_token().await {
+                Ok(access_token) => {
+                    for artist in new_artist_entries.iter_mut() {
+                        process_artist(&client, artist, access_token.clone()).await;
+                        current_library.push(artist.clone());
                     }
                 }
-                
-                if !new_album_entries.is_empty() {
-                    let albums_without_cover_count = new_album_entries.iter()
-                    .filter(|album| album.cover_url.is_empty())
-                    .count();
-                let log = format!("Searching cover art for {} albums...", albums_without_cover_count);
-                info!(log);
-                log_to_ws(log).await.unwrap();
-                
-                for artist in new_artist_entries.iter_mut() {
-                    for album in artist.albums.iter_mut() {
-                            process_album(&client, artist.name.clone(), album).await;
-                        }
-                    }
-                }
-            },
-            Err(_e) => {
-                let mut library_guard = library.lock().unwrap();
-                process_artists(&client, &mut *library_guard).await;
-                process_albums(&client, &mut *library_guard).await;
-            },
+                Err(e) => warn!("Failed to get access token. Error: {}", e),
+            }
         }
-        
-        //https://musicbrainz.org/ws/2/release/cbaf43b4-0d8f-4b58-9173-9fe7298e04e9?inc=aliases+artist-credits+labels+discids+recordings+release-groups+media+discids+recordings+artist-credits+isrcs+artist-rels+release-rels+url-rels+recording-rels+work-rels+label-rels+place-rels+event-rels+area-rels+instrument-rels+series-rels+work-rels&fmt=json
-        
+
+        if !new_album_entries.is_empty() {
+            let albums_without_cover_count = new_album_entries
+                .iter()
+                .filter(|modified_album| modified_album.album.cover_url.is_empty())
+                .count();
+            let log = format!(
+                "Searching cover art for {} albums...",
+                albums_without_cover_count
+            );
+            info!(log);
+            log_to_ws(log).await.unwrap();
+
+            for modified_album in new_album_entries.iter_mut() {
+                process_album(
+                    &client,
+                    modified_album.artist_name.clone(),
+                    &mut modified_album.album,
+                )
+                .await;
+                if let Some(artist) = current_library
+                    .iter_mut()
+                    .find(|a| a.id == modified_album.artist_id)
+                {
+                    match artist
+                        .albums
+                        .iter_mut()
+                        .find(|a| a.id == modified_album.album.id)
+                    {
+                        Some(existing_album) => *existing_album = modified_album.album.clone(),
+                        None => artist.albums.push(modified_album.album.clone()),
+                    }
+                }
+            }
+        }
+    }
+
+    //https://musicbrainz.org/ws/2/release/cbaf43b4-0d8f-4b58-9173-9fe7298e04e9?inc=aliases+artist-credits+labels+discids+recordings+release-groups+media+discids+recordings+artist-credits+isrcs+artist-rels+release-rels+url-rels+recording-rels+work-rels+label-rels+place-rels+event-rels+area-rels+instrument-rels+series-rels+work-rels&fmt=json
+
     let library_guard = library.lock().unwrap();
     let elapsed = now.elapsed().as_secs();
 
@@ -108,7 +138,13 @@ async fn process_library(path_to_library: web::Path<String>, ) -> impl Responder
     info!(log);
     log_to_ws(log).await.unwrap();
 
-    let json = serde_json::to_string(&*library_guard).unwrap();
+    let data_to_serialize = if current_library.is_empty() {
+        &*library_guard
+    } else {
+        &current_library
+    };
+
+    let json = serde_json::to_string(data_to_serialize).unwrap();
 
     save_config(&json).await.unwrap();
     HttpResponse::Ok()
@@ -116,17 +152,20 @@ async fn process_library(path_to_library: web::Path<String>, ) -> impl Responder
         .body(json)
 }
 
-
 #[derive(Deserialize)]
 struct BitrateQueryParams {
-    bitrate: u64
+    bitrate: u64,
 }
 
 #[get("/stream/{song}")]
-async fn stream_song(req: HttpRequest, path: web::Path<String>, bitrate: web::Query<BitrateQueryParams>) -> impl Responder {
+async fn stream_song(
+    req: HttpRequest,
+    path: web::Path<String>,
+    bitrate: web::Query<BitrateQueryParams>,
+) -> impl Responder {
     let song = path.into_inner();
     let bitrate = bitrate.into_inner().bitrate;
-    
+
     let file = fs::metadata(&song).await.unwrap();
     let song_file_size = file.len();
 
@@ -147,12 +186,15 @@ async fn stream_song(req: HttpRequest, path: web::Path<String>, bitrate: web::Qu
             let bytes = range.trim_start_matches("bytes=");
             let range_parts: Vec<&str> = bytes.split('-').collect();
             let start = range_parts[0].parse::<u64>().unwrap_or(0);
-            let end = range_parts.get(1).and_then(|s| s.parse::<u64>().ok()).unwrap_or(song_file_size - 1);
+            let end = range_parts
+                .get(1)
+                .and_then(|s| s.parse::<u64>().ok())
+                .unwrap_or(song_file_size - 1);
             (start, end)
-            },
-            None => (0, song_file_size - 1),
-            };
-            
+        }
+        None => (0, song_file_size - 1),
+    };
+
     let content_range = format!("bytes {}-{}/{}", start, end, song_file_size);
     let content_length = (end - start + 1).to_string();
 
@@ -172,7 +214,6 @@ async fn stream_song(req: HttpRequest, path: web::Path<String>, bitrate: web::Qu
             _ => "application/octet-stream",
         };
 
-
         HttpResponse::PartialContent()
             .append_header((header::CONTENT_TYPE, mime_type))
             .append_header((header::CONTENT_RANGE, content_range))
@@ -181,24 +222,33 @@ async fn stream_song(req: HttpRequest, path: web::Path<String>, bitrate: web::Qu
     } else {
         let audio_bitrate = tagged_file.properties().audio_bitrate().unwrap() as f64 * 1000.0 / 8.0;
         let start_time = (start) as f64 / audio_bitrate;
-        let duration = ((end - start)) as f64 / audio_bitrate;
+        let duration = (end - start) as f64 / audio_bitrate;
 
         let mut command = Command::new("ffmpeg")
             .args([
-                "-ss", &start_time.to_string(),
-                "-t", &duration.to_string(),
-                "-i", &song,
-                "-af", "loudnorm=I=-14:LRA=11:TP=-1",
-                "-ab", &format!("{}k", bitrate),
-                "-f", "mp3",
-                "pipe:1"
+                "-ss",
+                &start_time.to_string(),
+                "-t",
+                &duration.to_string(),
+                "-i",
+                &song,
+                "-af",
+                "loudnorm=I=-14:LRA=11:TP=-1",
+                "-ab",
+                &format!("{}k", bitrate),
+                "-f",
+                "mp3",
+                "pipe:1",
             ])
             .stdout(Stdio::piped())
             .spawn()
             .expect("Failed to start ffmpeg process");
-        
-        let stdout = command.stdout.take().expect("Failed to take stdout of ffmpeg");
-        
+
+        let stdout = command
+            .stdout
+            .take()
+            .expect("Failed to take stdout of ffmpeg");
+
         let stream = ReaderStream::new(stdout);
         let body_stream = stream.map(|result| {
             result.map(|bytes| bytes.into()).map_err(|e| {
@@ -206,7 +256,7 @@ async fn stream_song(req: HttpRequest, path: web::Path<String>, bitrate: web::Qu
                 actix_web::error::ErrorInternalServerError(e)
             })
         });
-        
+
         HttpResponse::Ok()
             .append_header((header::CONTENT_TYPE, "audio/mpeg"))
             .append_header((header::CONTENT_RANGE, content_range))
@@ -233,14 +283,15 @@ async fn index_library_no_cover_url(path: web::Path<String>) -> impl Responder {
     let json: String = serde_json::to_string(&*indexed_library.lock().unwrap()).unwrap();
 
     HttpResponse::Ok()
-    .content_type("application/json; charset=utf-8")
-    .body(json)
-
+        .content_type("application/json; charset=utf-8")
+        .body(json)
 }
 
 #[get("/test")]
 async fn test() -> impl Responder {
-    log_to_ws("Hit the test endpoint.".to_string()).await.unwrap();
+    log_to_ws("Hit the test endpoint.".to_string())
+        .await
+        .unwrap();
 
     HttpResponse::Ok()
 }
