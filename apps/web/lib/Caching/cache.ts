3d3a5177ac36e93924fac032a33315ecb0fa7ec9
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:26:11 +0100

    Feat(cache): Add Basic Cache Functions Using LocalStorage
    Introduced functions for setting, getting, and invalidating cache items with TTL support in localStorage.

---
+++ apps/web/lib/Caching/cache.ts
@@ -0,0 +1,30 @@
+const setCache = (key: string, value: any, ttl: number) => {
+  const now = new Date();
+  const item = {
+    value: value,
+    expiry: now.getTime() + ttl,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+const getCache = (key: string) => {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+};
+
+const invalidateCache = (key: string) => {
+  localStorage.removeItem(key);
+};
+
+export default setCache;
+export { getCache, invalidateCache };
+
