b88890073b42b7fb2d54ece1fe079492492b2528
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:04:35 +0100

    Refactor(history): Improve client-side history fetching on client

--- apps/web/app/(app)/history/page.tsx
+++ apps/web/app/(app)/history/page.tsx
@@ -1,51 +1,59 @@
-import GetListenHistory from "@/actions/History/GetListenHistory";
-import getServerIpAddress from "@/actions/System/GetIpAddress";
-import GetPort from "@/actions/System/GetPort";
+"use client"
+
+import getBaseURL from "@/lib/Server/getBaseURL";
+
+import { useState, useEffect } from "react";
 import BigCard from "@/components/Music/Card/BigCard";
-import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
+import { getListenHistory, getSongInfo } from "@music/sdk";
 import { ScrollArea } from "@music/ui/components/scroll-area";
-import fs from "fs";
+import { LibrarySong } from "@music/sdk/types";
+import getSession from "@/lib/Authentication/JWT/getSession";
 
-export default async function HistoryPage() {
-  const session = await getServerSession();
-  const username = session?.user.username;
-  let listenHistorySongs = await GetListenHistory(username ?? "");
+export default function HistoryPage() {
+  const [listenHistorySongs, setListenHistorySongs] = useState<LibrarySong[]>([]);
 
-  const serverIPAddress = await getServerIpAddress();
-  const port = await GetPort();
+  useEffect(() => {
+    const fetchListenHistory = async () => {
+      const session = getSession()
 
-  function imageToBase64(src: string) {
-    const image = fs.readFileSync(src);
-    const base64Image = Buffer.from(image).toString("base64");
-    return base64Image;
-  }
+      if (session) {
+        const listenHistoryItems = await getListenHistory(Number(session.sub));
+        const songDetailsPromises = listenHistoryItems.reverse().map(item => getSongInfo(item.song_id));
+        const songDetails = await Promise.all(songDetailsPromises);
+        
+        setListenHistorySongs(songDetails);
+      }
+    };
+  
+    fetchListenHistory();
+  }, []);
 
   return (
-      <>
-        <h1 className="text-3xl font-bold pt-20">History</h1>
-  
-        <ScrollArea>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 h-full w-full">
-            {listenHistorySongs.map((song, index) => (
-              <div className="h-64" key={index}>
-                <BigCard
-                  title={song.name}
-                  album={song.albumObject}
-                  artist={song.artistObject}
-                  imageSrc={
-                    song.image.length === 0
+    <>
+      <h1 className="text-3xl font-bold pt-20">History</h1>
+
+      <ScrollArea>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 h-full w-full">
+          {listenHistorySongs.map((song, index) => (
+            <div className="h-64" key={index}>
+              <BigCard
+                title={song.name}
+                album={song.album_object}
+                artist={song.artist_object}
+                imageSrc={
+                  song.album_object.cover_url.length === 0
                     ? "/snf.png"
-                    : `data:image/jpg;base64,${imageToBase64(song.image)}`
-                  }
-                  albumURL=""
-                  songURL={`http://${serverIPAddress}:${port}/server/stream/${encodeURIComponent(song.path)}?bitrate=0`}
-                  type="Song"
-                  song={song}
-                />
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
-      </>
-    );
+                    : `${getBaseURL()}/image/${encodeURIComponent(song.album_object.cover_url)}`
+                }
+                albumURL=""
+                songURL={`${getBaseURL()}/api/stream/${encodeURIComponent(song.path)}?bitrate=0`}
+                type="Song"
+                song={song}
+              />
+            </div>
+          ))}
+        </div>
+      </ScrollArea>
+    </>
+  );
 }
