68df4098d0c13cbb39ce22b43bfca707fbdc7217
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:21:08 +0100

    Refactor(ArtistCard): Update Image Handling, Adjust Layout
    Modified artist icon URL logic to use base server URL. Adjusted layout for better alignment and presentation. Updated imports for consistency.

--- apps/web/components/Music/Artist/ArtistCard.tsx
+++ apps/web/components/Music/Artist/ArtistCard.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image"
-import imageToBase64 from "@/actions/ImageToBase64"
-import type Artist from "@/types/Music/Artist"
-import type Album from "@/types/Music/Album"
-import Link from "next/link"
+import getBaseURL from "@/lib/Server/getBaseURL";
+import { Artist } from "@music/sdk/types";
+import Image from "next/image";
+import Link from "next/link";
 
 type ArtistCardProps = {
   artist: Artist
@@ -19,20 +18,19 @@ function formatFollowers(followers: number): string {
 }
 
 export default async function ArtistCard({ artist }: ArtistCardProps) {
-  const base64Image = await imageToBase64(artist.icon_url)
-  const artistIconURL = artist.icon_url.length === 0 ? "/snf.png" : `data:image/jpg;base64,${base64Image}`
+  const artistIconURL = artist.icon_url.length === 0 ? "/snf.png" : `${getBaseURL()}/image/${encodeURIComponent(artist.icon_url)}`
 
-return (
-  <Link href={`/artist/${artist.id}`}>
-    <div className="w-44 h-44 flex items-center justify-center">
-      <div className="flex flex-row items-center gap-2">
-        <Image width={256} height={256} src={artistIconURL} alt={`${artist.name} Image`} className="rounded-full"/>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-bold text-white overflow-hidden overflow-ellipsis whitespace-nowrap" title={artist.name}>{artist.name}</p>
-          <p className="font-bold text-white overflow-hidden overflow-ellipsis whitespace-nowrap" title={artist.name}>{formatFollowers(artist.followers)} Followers</p>
+  return (
+    <Link href={`/artist?id=${artist.id}`}>
+      <div className="w-44 h-64 flex items-center justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Image width={256} height={256} src={artistIconURL} alt={`${artist.name} Image`} className="rounded-full"/>
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-bold text-white overflow-hidden overflow-ellipsis whitespace-nowrap" title={artist.name}>{artist.name}</p>
+            <p className="font-bold text-white overflow-hidden overflow-ellipsis whitespace-nowrap" title={artist.name}>{formatFollowers(artist.followers)} Followers</p>
+          </div>
         </div>
       </div>
-    </div>
-  </Link>
-)
+    </Link>
+  )
 }
