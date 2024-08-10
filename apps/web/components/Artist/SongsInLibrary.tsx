daaa0f73cd684b05a6b4998812d288af13a1ff16
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:07:43 +0100

    Feat(SongsInLibrary): Refactor to enhance song fetching
    Refactored to use `getSession`, `getBaseURL`, and SDK API for fetching songs from playlists. Simplified loading component state logic and improved code structure.

--- apps/web/components/Artist/SongsInLibrary.tsx
+++ apps/web/components/Artist/SongsInLibrary.tsx
@@ -1,95 +1,75 @@
-import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
-import prisma from "@/prisma/prisma";
-import Album from "@/types/Music/Album";
-import Artist from "@/types/Music/Artist";
+import getSession from "@/lib/Authentication/JWT/getSession";
+import getBaseURL from "@/lib/Server/getBaseURL";
+import { getPlaylist, getPlaylists, getSongInfo } from "@music/sdk";
+import { LibrarySong } from "@music/sdk/types";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import ScrollButtons from "../Home/ScrollButtons";
 import BigCard from "../Music/Card/BigCard";
-import { ScrollArea, ScrollBar } from "@music/ui/components/scroll-area";
-import imageToBase64 from "@/actions/ImageToBase64";
-import getServerIpAddress from "@/actions/System/GetIpAddress";
-import GetPort from "@/actions/System/GetPort";
 
-type SongsInLibraryProps = {
-  allSongs: allSongs[]
-}
+async function getSongsFromYourLibrary(user_id: number, artist_id: string) {
+  const playlists = await getPlaylists(user_id);
 
-type allSongs = {
-  artistObject: Artist;
-  albumObject: Album;
-  album: string;
-  image: string;
-  id: string;
-  name: string;
-  artist: string;
-  contributing_artists: string[];
-  track_number: number;
-  path: string;
-  duration: number;
-}
-
-export default async function SongsInLibrary({ allSongs }: SongsInLibraryProps) {
-
-  const songIds = allSongs.map(song => song.id);
-
-  const session = await getServerSession()
-  const username = session?.user.username
-  const songsFromPlaylist = await prisma.playlist.findMany({
-    where: {
-      users: {
-        some: {
-          username
-        }
-      },
-      songs: {
-        some: {
-          id: {
-            in: songIds.map(String)
-          },
-        },
-      },
-    },
-    select: {
-      songs: {
-        select: {
-          id: true
-        }
-      }
-    }
+  const playlistSongIDsPromises = playlists.map(async (playlist) => {
+    const individualPlaylist = await getPlaylist(playlist.id);
+    return individualPlaylist.song_infos.map((songInfo) => songInfo.song_id);
   });
 
-  const songIdsFromPlaylist = songsFromPlaylist.flatMap(playlist => 
-    playlist.songs
-      .filter(song => songIds.includes(song.id))
-      .map(song => song.id)
+  const playlistSongIDsArrays = await Promise.all(playlistSongIDsPromises);
+  const playlistSongIDs = playlistSongIDsArrays.flat();
+
+  const songsDetailsPromises = playlistSongIDs.map((songID) => getSongInfo(String(songID)));
+
+  const songsDetails = await Promise.all(songsDetailsPromises);
+
+  const filteredSongsDetails = songsDetails.filter(song => String(song.artist_object.id) === artist_id);
+
+  return filteredSongsDetails;
+}
+
+export default function FromYourLibrary() {
+  const [librarySongs, setLibrarySongs] = useState<LibrarySong[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const searchParams = useSearchParams();
+  const id = searchParams?.get("id");
+
+  useEffect(() => {
+    const session = getSession();
+
+    async function fetchSongs() {
+      if (session && id) {
+        const songs = await getSongsFromYourLibrary(Number(session.sub), id);
+        setLibrarySongs(songs);
+        setLoading(false);
+      }
+    }
+
+    fetchSongs();
+  }, [id]);
+
+  return librarySongs.length > 0 && (
+    <ScrollButtons heading="In your Library">
+      <div className="flex flex-row justify-start items-start">
+        {librarySongs.map((song, index) => (
+          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-8">
+            <BigCard
+              title={song.name}
+              album={song.album_object}
+              artist={song.artist_object}
+              imageSrc={
+                song.album_object.cover_url.length === 0
+                ? "/snf.png"
+                : `${getBaseURL()}/image/${encodeURIComponent(song.album_object.cover_url)}`
+              }
+              albumURL=""
+              songURL={`${getBaseURL()}/api/stream/${encodeURIComponent(song.path)}`}
+              type="Song"
+              song={song}
+            />
+          </div>
+        ))}
+      </div>
+    </ScrollButtons>
   );
-
-  const songsInLibrary = allSongs.filter(song => songIdsFromPlaylist.includes(String(song.id)));
-
-
-  return songsInLibrary.length > 0 && (
-    <>
-      <p className="text-2xl text-bold">In your Library</p>
-      <ScrollArea className="w-full overflow-x-auto overflow-y-auto mb-4 h-64">
-        <div className="flex flex-row justify-start items-start">
-          {songsInLibrary.map(async (song, index) => (
-            <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-8">
-              <BigCard
-                title={song.name}
-                album={song.albumObject}
-                artist={song.artistObject}
-                imageSrc={
-                  song.image.length === 0
-                  ? "/snf.png"
-                  : `data:image/jpg;base64,${await imageToBase64(song.image)}`
-                }
-                albumURL=""
-                songURL={`http://${await getServerIpAddress()}:${await GetPort()}/server/stream/${encodeURIComponent(song.path)}`}
-                type="Song"
-                song={song}
-                />
-            </div>
-          ))}
-        </div>
-        <ScrollBar orientation="horizontal"/>
-      </ScrollArea>
-    </>
-  )}
+}
