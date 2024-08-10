5313ec9173476f584a87008ce9a97d3a2057ff72
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:21:50 +0100

    Feat(TopResultsCard): Refactor And Optimize Data Fetch
    Refactored code to use hooks for fetching and rendering artist, album, and song data. Simplified image URL handling and dynamic styling.

--- apps/web/components/Music/Card/Search/TopResultsCard.tsx
+++ apps/web/components/Music/Card/Search/TopResultsCard.tsx
@@ -1,51 +1,145 @@
-import React from 'react';
-import BigCard from '../BigCard';
+import getSession from '@/lib/Authentication/JWT/getSession';
+import getBaseURL from "@/lib/Server/getBaseURL";
+import { getAlbumInfo, getArtistInfo, getSongInfo, LibraryAlbum } from '@music/sdk';
+import { Artist, CombinedItem, LibrarySong } from '@music/sdk/types';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import ArtistCard from '../../Artist/ArtistCard';
-import { SearchResult } from 'minisearch';
-import imageToBase64 from '@/actions/ImageToBase64';
 import AlbumCard from '../Album/AlbumCard';
-import getServerIpAddress from '@/actions/System/GetIpAddress';
-import GetPort from '@/actions/System/GetPort';
-import getServerSession from '@/lib/Authentication/Sessions/GetServerSession';
+import BigCard from '../BigCard';
 
 type ResultCardProps = {
-  result: SearchResult
+  result: CombinedItem 
 }
 
-export default async function TopResultsCard({ result }: ResultCardProps) {
-  const session = await getServerSession()
-  const bitrate = session!.user.bitrate
+export default function TopResultsCard({ result }: ResultCardProps) {
+  const session = getSession()
 
-  if (result.type === 'Song') {
-    return (
-      <div key={result.id} className="flex flex-col items-center p-14">
-        <BigCard
-          artist={result.artist}
-          album={result.album}
-          songURL={`http://${await getServerIpAddress()}:${await GetPort()}/server/stream/${encodeURIComponent(result.song.path)}?bitrate=${bitrate}`}
-          title={result.name}
-          type="Song"
-          imageSrc={
-            result.album.cover_url.length == 0
-            ? "/snf.png"
-            : `data:image/jpg;base64,${await imageToBase64(result.album.cover_url)}`
-          }
-          albumURL=""
-          song={result.song}
-        />
-      </div>
-    );
-  } else if (result.type === 'Artist') {
-    return (
-      <div key={result.id} className="flex flex-col items-center p-14">
-        <ArtistCard artist={result.artist}/>
-      </div>
-    );
-  } else if (result.type === 'Album') {
-    return (
-      <div key={result.id} className="flex flex-col items-center p-14">
-        <AlbumCard album={result.album} artist={result.artist} />
-      </div>
-    );
-  }
+  const [artist, setArtist] = useState<Artist | null>(null)
+  const [album, setAlbum] = useState<LibraryAlbum | null>(null)
+  const [song, setSong] = useState<LibrarySong | null>(null)
+
+  useEffect(() => {
+    async function fetchInfo() {
+      if (result.item_type === "song") {
+        const song = await getSongInfo(result.id)
+        setSong(song)
+      }
+
+      if (result.item_type === "album") {
+        const album = await getAlbumInfo(result.id)
+        setAlbum(album)
+      }
+
+      if (result.item_type === "artist") {
+        const artist = await getArtistInfo(result.id)
+        setArtist(artist)
+      }
+    }
+
+    fetchInfo()
+  }, [result.id, result.item_type])
+
+const coverUrl = result.item_type === "song" 
+  ? song?.album_object.cover_url 
+  : result.item_type === "album" 
+  ? album?.cover_url 
+  : result.item_type === "artist" 
+  ? artist?.icon_url 
+  : "";
+
+  const imageSrc = `${getBaseURL()}/image/${encodeURIComponent(coverUrl || "")}?raw=true`;
+
+    if (result.item_type === 'song' && song) {
+      return (
+        <div key={result.id} className="relative flex flex-col items-center p-14" style={{ height: 350, width: 500 }}>
+          <Image
+            className="bg-cover bg-center blur-3xl"
+            src={imageSrc}
+            alt="Background Image"
+            height={1000}
+            width={1000}
+            style={{
+              backgroundColor: "#202020",
+              transition: "background background-color 0.5s ease",
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              filter: 'blur(10px) brightness(50%)',
+              zIndex: "10"
+            }}
+          />
+          <div className="relative z-10">
+            <BigCard
+              artist={song.artist_object}
+              album={song.album_object}
+              songURL={`${getBaseURL()}/api/stream/${encodeURIComponent(song.path)}?bitrate=${session && session.bitrate}`}
+              title={result.name}
+              type="Song"
+              imageSrc={
+                song.album_object.cover_url.length == 0
+                  ? "/snf.png"
+                  : `${getBaseURL()}/image/${encodeURIComponent(song.album_object.cover_url)}`
+              }
+              albumURL=""
+              song={song}
+            />
+          </div>
+        </div>
+      );
+    } else if (result.item_type === 'artist' && artist) {
+      return (
+        <div key={result.id} className="relative flex flex-col items-center p-14" style={{ height: 350, width: 500 }}>
+          <Image
+            className="bg-cover bg-center blur-3xl"
+            src={imageSrc}
+            alt="Background Image"
+            height={1000}
+            width={1000}
+            style={{
+              backgroundColor: "#202020",
+              transition: "background background-color 0.5s ease",
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              filter: 'blur(10px) brightness(50%)',
+              zIndex: "10"
+            }}
+          />
+          <div className="relative z-10">
+            <ArtistCard artist={artist} />
+          </div>
+        </div>
+      );
+    } else if (result.item_type === 'album' && album) {
+      return (
+        <div key={result.id} className="relative flex flex-col items-center p-14 h-14 w-14" style={{ height: 350, width: 500 }}>
+          <Image
+            className="bg-cover bg-center blur-3xl"
+            src={imageSrc}
+            alt="Background Image"
+            height={1000}
+            width={1000}
+            style={{
+              backgroundColor: "#202020",
+              transition: "background background-color 0.5s ease",
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              filter: 'blur(10px) brightness(50%)',
+              zIndex: "10"
+            }}
+          />
+          <div className="relative z-10">
+            <AlbumCard album={album} artist={album.artist_object} />
+          </div>
+        </div>
+      );
+    }
 }
