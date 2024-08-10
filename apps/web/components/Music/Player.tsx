9a3a40f969d4ab65c34fab9c474065cef55c067b
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:20:41 +0100

    Refactor(Player): Update Imports & Background Image Styling
    Organised imports alphabetically and refactored background image handling using the Image component. Updated link path parameters and fixed styles for song name display and image container.

--- apps/web/components/Music/Player.tsx
+++ apps/web/components/Music/Player.tsx
@@ -1,32 +1,28 @@
-"use client";
+"use client"
 
 import Image from "next/image";
 
-import { useState, useRef, useContext, useEffect } from "react";
-import { PanelContext } from "./Queue/QueuePanelContext";
-import IconPause from "../Icons/Pause";
-import IconPlay from "../Icons/Play";
-import PlusCircle from "../Icons/PlusCircle";
-import CheckCircle from "../Icons/CheckCircle";
-import ArrowPath from "../Icons/ArrowPath";
-import SpeakerXMark from "../Icons/SpeakerXMark";
-import SpeakerWave from "../Icons/SpeakerWave";
-import { BookAudioIcon, MicVocal, SkipBack, SkipForward } from "lucide-react";
-import IconQueue from "../Icons/IconQueue";
-import { usePlayer } from "./Player/usePlayer";
-import Head, { defaultHead } from "next/head"
-import AddToPlaylistDropdown from "./Player/AddToPlaylistDropdown";
 import {
   Slider,
   SliderRange,
   SliderThumb,
   SliderTrack,
 } from "@music/ui/components/slider";
-import Link from "next/link";
-import { LyricsContext } from "../Lyrics/LyricsOverlayContext";
-import { AIContext } from "../AI/AIOverlayContext";
-import { useGradientHover } from "../Providers/GradientHoverProvider";
 import { FastAverageColor } from "fast-average-color";
+import { BookAudioIcon, MicVocal, SkipBack, SkipForward } from "lucide-react";
+import Link from "next/link";
+import { useContext, useEffect, useRef, useState } from "react";
+import { AIContext } from "../AI/AIOverlayContext";
+import ArrowPath from "../Icons/ArrowPath";
+import IconQueue from "../Icons/IconQueue";
+import IconPause from "../Icons/Pause";
+import IconPlay from "../Icons/Play";
+import SpeakerWave from "../Icons/SpeakerWave";
+import SpeakerXMark from "../Icons/SpeakerXMark";
+import { LyricsContext } from "../Lyrics/LyricsOverlayContext";
+import { useGradientHover } from "../Providers/GradientHoverProvider";
+import { usePlayer } from "./Player/usePlayer";
+import { PanelContext } from "./Queue/QueuePanelContext";
 
 export default function Player() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
@@ -91,7 +87,24 @@ export default function Player() {
         backgroundColor: "#212121",
         transition: 'background-color 0.5s ease',
       }}>
-      <div className="bg-cover bg-center blur-3xl" style={{ backgroundImage: `${isPlaying ? `url(${imageSrc})` : "none"}`, backgroundColor: isPlaying ? "none" : "#202020", transition: "background background-color 0.5s ease", position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', filter: 'blur(96px) brightness(50%)', zIndex: -1 }} />
+      <Image
+        className="bg-cover bg-center blur-3xl"
+        src={isPlaying ? imageSrc : ""}
+        alt="Background Image"
+        height={1000}
+        width={1000}
+        style={{
+          backgroundColor: isPlaying ? "none" : "#202020",
+          transition: "background background-color 0.5s ease",
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          filter: 'blur(96px) brightness(50%)',
+          zIndex: -1
+        }}
+      />
     
       <section className="flex flex-col md:flex-row items-center justify-between gap-3 w-full md:w-1/4">
         <div className="flex items-center w-full md:w-1/2 justify-center">
@@ -116,7 +129,7 @@ export default function Player() {
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-3">
-            <div className="w-12 h-12 md:w-20 md:h-20 bg-gray-500 rounded-md">
+            <div className="w-24 h-12 md:w-20 md:h-20 bg-gray-500 rounded-md">
               <Image
                 alt={song.name + "Image"}
                 src={imageSrc}
@@ -126,13 +139,13 @@ export default function Player() {
             </div>
             <div className="w-32 md:overflow-hidden">
               <p
-                className={`whitespace-nowrap ${song.name.length > 15 ? "md:animate-marquee" : ""}`}
-                title={song.name.length > 15 ? song.name : ""}
+                className={`${song.name.length > 30 && "whitespace-nowrap"} ${song.name.length > 30 ? "md:animate-marquee" : ""}`}
+                title={song.name.length > 30 ? song.name : ""}
               >
-                <Link href={`/album/${album.id}`}>{song.name}</Link>
+                <Link href={`/album?id=${album.id}`}>{song.name}</Link>
               </p>
               <p className="text-xs text-gray-400">
-                <Link href={`/artist/${artist.id}`}>{artist.name}</Link>
+                <Link href={`/artist?id=${artist.id}`}>{artist.name}</Link>
               </p>
             </div>
           </div>
