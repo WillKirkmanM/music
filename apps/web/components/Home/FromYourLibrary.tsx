32c83effb4149576382c63c723d4edea763a89cc
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-10 17:16:22 +0100

    Refactor(FromYourLibrary): Switch To Client-Side Rendering
    Refactored to use client-side rendering with React hooks for fetching songs from the user's library, added caching, and updated song URLs to use `getBaseURL`. Removed unused imports.

--- apps/web/components/Home/FromYourLibrary.tsx
+++ apps/web/components/Home/FromYourLibrary.tsx
@@ -1,94 +1,84 @@
+"use client"
+
+import getBaseURL from "@/lib/Server/getBaseURL";
+
+import getSession from "@/lib/Authentication/JWT/getSession";
+import setCache, { getCache } from "@/lib/Caching/cache";
+import { getPlaylist, getPlaylists, getSongInfo } from "@music/sdk";
+import { LibrarySong } from "@music/sdk/types";
+import { useEffect, useState } from "react";
 import BigCard from "../Music/Card/BigCard";
-import fs from "fs";
-import { ScrollArea, ScrollBar } from "@music/ui/components/scroll-area"
-import type { Library } from "@/types/Music/Library";
-import getServerIpAddress from "@/actions/System/GetIpAddress";
-import GetPort from "@/actions/System/GetPort";
-import { unstable_cache as cache } from "next/cache";
-import getServerSession from "@/lib/Authentication/Sessions/GetServerSession";
-import prisma from "@/prisma/prisma";
-import Song from "@/types/Music/Song";
-import Album from "@/types/Music/Album";
-import Artist from "@/types/Music/Artist";
+import ScrollButtons from "./ScrollButtons";
 
-export const revalidate = 3600
+async function getSongsFromYourLibrary(user_id: number) {
+  const playlists = await getPlaylists(user_id);
 
-async function getSongsFromYourLibrary() {
-  const session = await getServerSession();
-  const username = session?.user.username;
-  const songsFromAllPlaylists = await prisma.playlist.findMany({
-    where: {
-      users: {
-        some: {
-          username
-        }
-      }
-    },
-    select: {
-      songs: {
-        select: {
-          id: true,
-        }
-      }
-    },
-    take: 10,
+  const playlistSongIDsPromises = playlists.map(async (playlist) => {
+    const individualPlaylist = await getPlaylist(playlist.id);
+    return individualPlaylist.song_infos.map((songInfo) => songInfo.song_id);
   });
 
-  const playlistSongIDs = songsFromAllPlaylists.flatMap(playlist => playlist.songs.map(song => song.id));
+  const playlistSongIDsArrays = await Promise.all(playlistSongIDsPromises);
+  const playlistSongIDs = playlistSongIDsArrays.flat();
 
-  const serverIPAddress = await getServerIpAddress();
-  const port = await GetPort();
-  const detailedSongsPromises = playlistSongIDs.map(async (songId) => {
-    const songRequest = await fetch(`http://${serverIPAddress}:${port}/server/song/info/${songId}`);
-    const detailedSong = await songRequest.json();
-    return detailedSong;
-  });
+  const songsDetailsPromises = playlistSongIDs.map((songID) => getSongInfo(String(songID)));
 
-  const detailedSongs = await Promise.all(detailedSongsPromises);
+  const songsDetails = await Promise.all(songsDetailsPromises);
 
-  return detailedSongs;
+  return songsDetails;
 }
 
-// const getCachedSongsFromYourLibrary = cache(
-//   async () => await getSongsFromYourLibrary(),
-//   ['from-your-library'],
-//   { revalidate: 300, tags: ["from-your-library"] }
-// );
+export default function FromYourLibrary() {
+  const [librarySongs, setLibrarySongs] = useState<LibrarySong[]>([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const session = getSession();
+  
+    async function fetchSongs() {
+      const cachedData = getCache("fromYourLibrary");
+  
+      if (cachedData) {
+        setLibrarySongs(cachedData);
+        setLoading(false);
+      } else {
+        if (session) {
+          const songs = await getSongsFromYourLibrary(Number(session.sub));
+          setLibrarySongs(songs);
+          setLoading(false);
+          setCache("fromYourLibrary", songs, 3600000);
+        }
+      }
+    }
+  
+    fetchSongs();
+  }, []);
 
+  if (loading) return null;
+  if (!librarySongs || librarySongs.length === 0) return null;
 
-export default async function FromYourLibrary() {
-  // let librarySongs = await getCachedSongsFromYourLibrary()
-  let librarySongs = await getSongsFromYourLibrary()
-
-  const serverIPAddress = await getServerIpAddress()
-  const port = await GetPort()
-
-  return librarySongs.length > 0 && (
-    <>
-      <h1 className="flex align-start text-3xl font-bold pb-8">From Your Library</h1>
-      <ScrollArea className="w-full overflow-x-auto overflow-y-auto h-80 pb-20">
-        <div className="flex flex-row">
-          {librarySongs.map((song: Song & { album_object: Album, artist_object: Artist, image: string } , index) => (
-            <div className="mr-20" key={index}>
-              <BigCard
-                title={song.name}
-                album={song.album_object}
-                artist={song.artist_object}
-                imageSrc={
-                  song.album_object.cover_url.length === 0
+  return (
+    <ScrollButtons heading="From Your Library">
+      <div className="flex flex-row">
+        {librarySongs.map((song, index) => (
+          <div className="mr-20" key={index}>
+            <BigCard
+              title={song.name}
+              album={song.album_object}
+              artist={song.artist_object}
+              imageSrc={
+                song.album_object.cover_url.length === 0
                   ? "/snf.png"
-                  : `http://${serverIPAddress}:${port}/server/image/${encodeURIComponent(song.album_object.cover_url)}`
-                }
-                albumURL=""
-                songURL={`http://${serverIPAddress}:${port}/server/stream/${encodeURIComponent(song.path)}?bitrate=0`}
-                type="Song"
-                song={song}
-                />
-            </div>
-          ))}
-        </div>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
-    </>
+                  : `${getBaseURL()}/image/${encodeURIComponent(song.album_object.cover_url)}`
+              }
+              albumURL=""
+              songURL={`${getBaseURL()}/api/stream/${encodeURIComponent(song.path)}?bitrate=0`}
+              type="Song"
+              song={song}
+            />
+          </div>
+        ))}
+      </div>
+    </ScrollButtons>
   );
 }
