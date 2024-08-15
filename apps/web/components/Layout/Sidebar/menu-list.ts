9cc33506eb706d5fb1decaa0577cb5aa9fdfe09f
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-15 10:24:45 +0100

    Feat(menu-list): Added Sidebar Menu List Component
    Created a new component for sidebar menus with group and submenu support, leveraging lucide-react icons for visual elements.

---
+++ apps/web/components/Layout/Sidebar/menu-list.ts
@@ -0,0 +1,57 @@
+import {
+  Tag,
+  Users,
+  Settings,
+  Bookmark,
+  SquarePen,
+  LayoutGrid,
+  LucideIcon
+} from "lucide-react";
+
+type Submenu = {
+  href: string;
+  label: string;
+  active: boolean;
+};
+
+type Menu = {
+  href: string;
+  label: string;
+  active: boolean;
+  icon: LucideIcon
+  submenus: Submenu[];
+};
+
+type Group = {
+  groupLabel: string;
+  menus: Menu[];
+};
+
+export function getMenuList(pathname: string): Group[] {
+  return [
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/dashboard",
+          label: "Dashboard",
+          active: pathname.includes("/dashboard"),
+          icon: LayoutGrid,
+          submenus: []
+        }
+      ]
+    },
+    {
+      groupLabel: "Access",
+      menus: [
+        {
+          href: "/admin/users",
+          label: "Users",
+          active: pathname.includes("/users"),
+          icon: Users,
+          submenus: []
+        },
+      ]
+    },
+  ];
+}
