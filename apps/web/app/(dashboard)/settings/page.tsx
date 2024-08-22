5b2e029a04540a8dc7aed60b97350c988da05b28
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-22 19:18:41 +0100

    Feat(dashboard): Add Profile Picture

--- apps/web/app/(dashboard)/settings/page.tsx
+++ apps/web/app/(dashboard)/settings/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import ChangeBitrate from "@/components/User/ChangeBitrate"
+import { ChangePassword } from "@/components/User/ChangePassword"
 import getSession from "@/lib/Authentication/JWT/getSession"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { uploadProfilePicture } from "@music/sdk"
+import { uploadProfilePicture, getProfilePicture } from "@music/sdk"
 import { Button } from "@music/ui/components/button"
+import Image from "next/image"
 import {
   Form,
   FormControl,
@@ -12,7 +15,8 @@ import {
   FormMessage,
 } from "@music/ui/components/form"
 import { Input } from "@music/ui/components/input"
-import { useState } from "react"
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar"
+import { useState, useEffect } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
@@ -30,6 +34,24 @@ export default function SettingsPage() {
 
   const [file, setFile] = useState<File | null>(null)
   const [message, setMessage] = useState<string | null>(null)
+  const [profilePicture, setProfilePicture] = useState<string | null>(null)
+  const [username, setUsername] = useState<string>("")
+
+  useEffect(() => {
+    const fetchSessionAndProfilePicture = async () => {
+      const session = getSession()
+      if (session) {
+        setUsername(session.username)
+        const profilePic = await getProfilePicture(Number(session.sub))
+        if (profilePic) {
+          setProfilePicture(URL.createObjectURL(profilePic))
+        } else {
+          setProfilePicture(null)
+        }
+      }
+    }
+    fetchSessionAndProfilePicture()
+  }, [])
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     const session = getSession()
@@ -48,36 +70,69 @@ export default function SettingsPage() {
   }
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0] || null;
-    setFile(selectedFile);
-  };
+    const selectedFile = e.target.files?.[0] || null
+    setFile(selectedFile)
+    if (selectedFile) {
+      setProfilePicture(URL.createObjectURL(selectedFile))
+    }
+  }
 
   const handleSubmit = () => {
-    form.handleSubmit(onSubmit)();
-  };
+    form.handleSubmit(onSubmit)()
+  }
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="w-1/2">
-        <Form {...form}>
-          <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className="space-y-6">
-            <FormItem>
-              <FormLabel htmlFor="picture">Picture</FormLabel>
-              <FormControl>
-                <Input
-                  id="picture"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFileChange}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-            {message && <p>{message}</p>}
-            <Button type="submit">Upload</Button>
-          </form>
-        </Form>
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 ml-1/4">
+        <div className="w-1/2">
+          <Form {...form}>
+            <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className="space-y-6 text-white">
+              <FormItem>
+                <FormLabel htmlFor="picture" className="block text-2xl font-medium text-white">Profile Picture</FormLabel>
+                <div className="flex items-center space-x-4">
+                  <FormControl>
+                    <div>
+                      <input
+                        id="picture"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                        className="hidden"
+                      />
+                      <label htmlFor="picture" className="cursor-pointer">
+                        <div className="mr-4 w-16 h-16 rounded-full overflow-hidden flex-shrink-0">
+                          {profilePicture ? (
+                            <Image
+                              src={profilePicture}
+                              alt="User Profile Picture"
+                              width={64}
+                              height={64}
+                              className="w-full h-full object-cover rounded-full"
+                            />
+                          ) : (
+                            <div className="w-full h-full flex items-center justify-center bg-gray-600 text-white rounded-full">
+                              {username.substring(0, 2).toUpperCase()}
+                            </div>
+                          )}
+                        </div>
+                      </label>
+                    </div>
+                  </FormControl>
+                </div>
+                <FormMessage className="text-sm text-red-600" />
+              </FormItem>
+              {message && <p className="text-center text-red-600">{message}</p>}
+              <Button type="submit" className="w-1/3 px-4 py-2 mt-6 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Upload</Button>
+            </form>
+          </Form>
+          <div className="mt-6">
+            <ChangeBitrate />
+          </div>
+          <div className="mt-6">
+            <ChangePassword />
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
