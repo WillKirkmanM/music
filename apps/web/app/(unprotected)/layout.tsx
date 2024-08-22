0e876dea72db00ff799fb95e6d9f8503db5102f9
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-22 19:20:14 +0100

    Fix(root): Format

--- apps/web/app/(unprotected)/layout.tsx
+++ apps/web/app/(unprotected)/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontSans.variable
         )}>
           <div className="flex flex-row text-white items-center font-bold pl-4 pt-3 fixed">
-            <Image src={pl} alt="ParsonLabs" height={30} width={30}/>
+            <Image src={pl} alt="ParsonLabs" height={30} width={30} />
             <div className="md:text-xl text-white font-bold pl-3">
-                ParsonLabs Music
+              ParsonLabs Music
             </div>
           </div>
         {children}
