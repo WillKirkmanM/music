712387dca89b414c3eb51ab984dfff9b009e7454
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-22 19:18:20 +0100

    Feat(layout): Added Sidebar

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,8 +1,12 @@
 import "@music/ui/globals.css"
 import { Inter as FontSans } from "next/font/google"
 
+import pl from "@/assets/pl-tp.png"
+import SettingsSidebar from "@/components/Layout/Settings/Sidebar"
 import { cn } from "@music/ui/lib/utils"
+import Link from "next/link"
 import { Metadata } from "next"
+import Image from "next/image"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,9 +27,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased ",
+          "min-h-screen bg-background font-sans antialiased bg-gray-900 texxt-white",
           fontSans.variable
         )}>
+          <Link href="/home">
+          <div className="flex flex-row text-white items-center font-bold pl-4 pt-3 fixed z-50">
+            <Image src={pl} alt="ParsonLabs" height={30} width={30}/>
+            <div className="md:text-xl text-white font-bold pl-3">
+                ParsonLabs Music
+            </div>
+          </div>
+          </Link>
+          <div className="fixed left-0 top-0 h-full w-1/4 flex items-center justify-center">
+            <SettingsSidebar />
+          </div>
           {children}
       </body>
     </html>
