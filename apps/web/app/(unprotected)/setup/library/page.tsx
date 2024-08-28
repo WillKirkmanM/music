ead65bc6b08019862fd0380c83eb73030fbc89e5
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-08-28 16:37:11 +0100

    Feat(setup/library): Check hostname if baseURL is not defined

--- apps/web/app/(unprotected)/setup/library/page.tsx
+++ apps/web/app/(unprotected)/setup/library/page.tsx
@@ -6,12 +6,13 @@ import React, { useState, useEffect } from "react";
 import useWebSocket from "react-use-websocket";
 import { ScrollArea, ScrollBar } from "@music/ui/components/scroll-area";
 import FileBrowser from "@/components/FileBrowser/FileBrowser";
+import Link from "next/link";
 
 export default function SetupLibrary() {
   const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
   
   const baseUrl = getBaseURL()?.replace(/^https?:\/\//, '');
-  const socketUrl = `ws://${baseUrl}/ws`;
+  const socketUrl = `ws://${baseUrl ?? window.location.hostname}/ws`;
   const { lastMessage } = useWebSocket(socketUrl);
   
   useEffect(() => {
@@ -24,27 +25,29 @@ export default function SetupLibrary() {
   }, [lastMessage]);
 
   return (
-    <> 
-      <p className="text-white text-center text-4xl py-14">Link your Music Library</p>
-
-      <div className="flex justify-between text-white">
-        <div className="w-1/2">
-          <FileBrowser />
-        </div>
-
-        <div className="w-1/2 bg-gray-800 p-4 rounded-md">
-          <h2 className="text-2xl flex justify-center pb-4">Logs</h2>
-          <div className="flex justify-center items-center">
-            <ScrollArea className="h-72 w-full rounded-md border bg-gray-700 p-4">
-              <ul className="py-2">
-                {messageHistory.map((message) => (
-                  <p key={message.data} className="text-white">{message.data}</p>
-                ))}
-              </ul>
-              <ScrollBar />
-            </ScrollArea>
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen text-white py-28">
+        <div className="flex w-full justify-center mb-8">
+          <div className="w-1/2 mr-4">
+            <FileBrowser />
+          </div>
+          <div className="w-1/2 bg-gray-800 p-4 rounded-md">
+            <h2 className="text-2xl flex justify-center pb-4">Logs</h2>
+            <div className="flex justify-center items-center">
+              <ScrollArea className="h-72 w-full rounded-md border bg-gray-700 p-4">
+                <ul className="py-2">
+                  {messageHistory.map((message) => (
+                    <p key={message.data} className="text-white">{message.data}</p>
+                  ))}
+                </ul>
+                <ScrollBar />
+              </ScrollArea>
+            </div>
           </div>
         </div>
+        <Link href="/setup/complete" className="underline text-white">
+          Continue
+        </Link>
       </div>
     </>
   );
