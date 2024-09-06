2c2e4a4e0dd1b3c45c4ebfd08e2ae05e678ff5b8
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-06 20:26:42 +0100

    Feat(home): Add GenreButtons and Music Video Card

--- apps/web/app/(app)/home/page.tsx
+++ apps/web/app/(app)/home/page.tsx
@@ -3,12 +3,16 @@
 import FromYourLibrary from "@/components/Home/FromYourLibrary";
 import LandingCarousel from "@/components/Home/LandingCarousel";
 import ListenAgain from "@/components/Home/ListenAgain";
+import MusicVideos from "@/components/Home/MusicVideos";
 import RandomSongs from "@/components/Home/RandomSongs";
 import RecommendedAlbums from "@/components/Home/RecommendedAlbums";
+import GenreButtons from "@/components/Layout/GenreButtons";
 
 export default function Home() {
   return (
-      <div className="min-h-screen pt-28 pb-20">
+    <div className="min-h-screen pt-14 pb-20">
+
+      <GenreButtons>
         <div className="pb-5">
           <LandingCarousel />
         </div>
@@ -17,6 +21,8 @@ export default function Home() {
         <RecommendedAlbums />
         <RandomSongs />
         <FromYourLibrary />
-      </div>
+        <MusicVideos />
+      </GenreButtons> 
+    </div>
   );
 }
