ce71a8eab1d4352de17942118a225dbd63779055
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-12 11:57:35 +0100

    Fix(websocket.rs): Premature Erroring Handling with Logging Websockets

--- crates/backend/src/utils/websocket.rs
+++ crates/backend/src/utils/websocket.rs
@@ -16,14 +16,14 @@ pub async fn ws(req: HttpRequest, body: web::Payload) -> Result<HttpResponse, Ot
     Ok(response)
 }
 
-pub async fn log_to_ws(message: impl Into<String>) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn log_to_ws(message: impl Into<String>) {
     let message = message.into();
     let mut session_lock = GLOBAL_SESSION.lock().unwrap();
     if let Some(session) = session_lock.as_mut() {
-        session.text(&message).await?;
+        if let Err(e) = session.text(&message).await {
+            info!("Failed to send message: {}", e);
+        }
     } else {
         info!("No active session found, message not sent");
-        return Ok(());
     }
-    Ok(())
 }
