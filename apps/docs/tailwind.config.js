605eabc93a65ffaaabd545fabc6077efe2ba64a5
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-12 19:10:00 +0100

    Refactor(tailwind.config): Reorder Presets And Content Paths

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -2,14 +2,12 @@ import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  presets: [createPreset()],
   content: [
-    '../../node_modules/fumadocs-ui/dist/**/*.js',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './content/**/*.{md,mdx}',
     './mdx-components.{ts,tsx}',
-    './node_modules/fumadocs-ui/dist/**/*.js',
+    '../../node_modules/fumadocs-ui/dist/**/*.js',
   ],
+  presets: [createPreset()],
 };
