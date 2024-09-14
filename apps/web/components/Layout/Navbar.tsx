33b40099255fdd97ddc64252fc1c05e839b78cfa
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-15 00:41:53 +0100

    Feat(Navbar): Add Explore, Library, Social

--- apps/web/components/Layout/Navbar.tsx
+++ apps/web/components/Layout/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import pl from "@/assets/pl-tp.png";
 import { Button } from '@music/ui/components/button';
 import { Menu } from 'lucide-react';
+import Image from 'next/image';
 import Link from "next/link";
 import { useContext, useState } from 'react';
 import { LyricsContext } from '../Lyrics/LyricsOverlayContext';
@@ -18,19 +20,22 @@ export default function NavBar() {
 
   return (
     <nav className={`fixed w-full top-0 p-4 flex items-center justify-between gap-2 box-border h-16 transition-colors duration-200 ${onTopOfPage || areLyricsVisible ? "bg-transparent" : "bg-black border-b border-gray-500"}  ${isSearchActive && 'items-stretch'}`}>
-    <div className={`${isSearchActive ? 'hidden' : 'flex'} md:flex items-center`}>
-      <Button onClick={toggleSidebar} variant="ghost" className="pr-4">
-        <Menu />
-      </Button>
-      <Link href="/home" onClick={() => setLyricsVisible(false)}>
-        <div className="md:text-lg text-white font-bold hover:text-gray-300">
-          ParsonLabs Music
+      <div className={`${isSearchActive ? 'hidden' : 'flex'} md:flex items-center`}>
+        <Button onClick={toggleSidebar} variant="ghost" className="pr-4">
+          <Menu />
+        </Button>
+        <div className="flex items-center">
+          <Image src={pl} alt="alt" className="mr-2" width={25} height={25}/>
+          <Link href="/home" onClick={() => setLyricsVisible(false)}>
+            <div className="md:text-lg text-white font-bold hover:text-gray-300">
+              ParsonLabs Music
+            </div>
+          </Link>
         </div>
-      </Link>
-    </div>
-
+      </div>
+  
       <SearchBar isSearchActive={isSearchActive} setIsSearchActive={setIsSearchActive} />
-
+  
       <div className={`${isSearchActive ? 'hidden' : 'flex'} gap-3 md:flex ${(onTopOfPage || areLyricsVisible) && "opacity-35"}`}>
         <NavbarProfilePicture />
       </div>
