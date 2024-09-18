0b8f78dce70c86fbb324eab5fb6a507ea0ce4bee
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-18 21:41:11 +0100

    Refactor(FromYourLibrary): Replace BigCard with SongCard

--- apps/web/components/Home/FromYourLibrary.tsx
+++ apps/web/components/Home/FromYourLibrary.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import getBaseURL from "@/lib/Server/getBaseURL";
 import getSession from "@/lib/Authentication/JWT/getSession";
 import setCache, { getCache } from "@/lib/Caching/cache";
 import { getPlaylist, getPlaylists, getSongInfo } from "@music/sdk";
 import { LibrarySong } from "@music/sdk/types";
 import { useEffect, useState } from "react";
-import BigCard from "../Music/Card/BigCard";
+import SongCard from "../Music/Card/SongCard";
 import ScrollButtons from "./ScrollButtons";
 
 async function getSongsFromYourLibrary(user_id: number, genre?: string) {
@@ -73,23 +72,10 @@ export default function FromYourLibrary({ genre }: FromYourLibraryProps) {
 
   return (
     <ScrollButtons heading="From Your Library">
-      <div className="flex flex-row">
+      <div className="flex flex-row pb-28">
         {librarySongs.map((song, index) => (
           <div className="mr-20" key={index}>
-            <BigCard
-              title={song.name}
-              album={song.album_object}
-              artist={song.artist_object}
-              imageSrc={
-                song.album_object.cover_url.length === 0
-                  ? "/snf.png"
-                  : `${getBaseURL()}/image/${encodeURIComponent(song.album_object.cover_url)}`
-              }
-              albumURL=""
-              songURL={`${getBaseURL()}/api/stream/${encodeURIComponent(song.path)}?bitrate=0`}
-              type="Song"
-              song={song}
-            />
+            <SongCard album_cover={song.album_object.cover_url} album_id={song.album_object.id} album_name={song.album_object.name} artist_id={song.artist_object.id} artist_name={song.artist} path={song.path} song_id={song.id} song_name={song.name} />
           </div>
         ))}
       </div>
