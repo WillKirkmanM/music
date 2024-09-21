f8571a545f4eef64c889480d2947d812da47ad91
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-09-21 14:16:21 +0100

    Feat(PlaylistCard): Add Optional Displays for both Cover & Artist

--- apps/web/components/Music/Playlist/PlaylistCard.tsx
+++ apps/web/components/Music/Playlist/PlaylistCard.tsx
@@ -5,18 +5,34 @@ import Link from "next/link";
 
 type PlaylistCardProps = {
   song: LibrarySong,
-  coverURL: string
-  artist: Artist
-  album: Album
+  coverURL: string,
+  artist: Artist,
+  album: Album,
+  showCover?: boolean,
+  showArtist?: boolean
 }
 
-export default function PlaylistCard({ song, coverURL, artist, album }: PlaylistCardProps) {
+export default function PlaylistCard({ song, coverURL, artist, album, showCover = true, showArtist = true }: PlaylistCardProps) {
   return (
     <div className="flex items-center">
-      <Image src={`${getBaseURL()}/image/${encodeURIComponent(coverURL)}`} alt={song.name + " Image"} width={64} height={64} className="rounded" />
-      <div className="flex flex-col ml-4">
-        <Link onClick={(e) => e.stopPropagation()} href={`/album?id=${album.id}`}><p>{song.name}</p></Link>
-        <Link onClick={(e) => e.stopPropagation()} href={`/artist?id=${artist.id}`}><p>{song.artist}</p></Link>
+      {showCover && (
+        <Image
+          src={`${getBaseURL()}/image/${encodeURIComponent(coverURL)}`}
+          alt={song.name + " Image"}
+          width={64}
+          height={64}
+          className="rounded"
+        />
+      )}
+      <div className="flex flex-col">
+        <Link onClick={(e) => e.stopPropagation()} href={`/album?id=${album.id}`}>
+          <p>{song.name}</p>
+        </Link>
+        {showArtist && (
+          <Link onClick={(e) => e.stopPropagation()} href={`/artist?id=${artist.id}`}>
+            <p>{artist.name}</p>
+          </Link>
+        )}
       </div>
     </div>
   )
