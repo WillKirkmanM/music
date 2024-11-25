c30ebe6395920a25b0ad3632f1ae64aa32c1c521
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-11-25 16:44:58 +0000

    Fix(FromYourLibrary.tsx): Fix Type Errors by Coercing to LibrarySong

--- apps/web/components/Home/FromYourLibrary.tsx
+++ apps/web/components/Home/FromYourLibrary.tsx
@@ -22,7 +22,7 @@ async function getSongsFromYourLibrary(user_id: number, genre?: string) {
 
   const songsDetailsPromises = playlistSongIDs.map((songID) => getSongInfo(String(songID)));
 
-  const songsDetails = await Promise.all(songsDetailsPromises);
+  const songsDetails = await Promise.all(songsDetailsPromises) as LibrarySong[];
 
   if (genre) {
     return songsDetails.filter(song => {
