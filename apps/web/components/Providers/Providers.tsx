c951ad853ed92c2fd337b9407e241b6cfed92bb0
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-01-25 23:10:00 +0000

    Feat(Providers.tsx): Add React Query Provider

--- apps/web/components/Providers/Providers.tsx
+++ apps/web/components/Providers/Providers.tsx
@@ -1,41 +1,62 @@
-import { ReactNode } from 'react';
-import AIOverlayProvider from '../AI/AIOverlayContext';
-import LyricsOverlayProvider from '../Lyrics/LyricsOverlayContext';
-import { PlayerProvider } from '../Music/Player/usePlayer';
-import PanelProvider from '../Music/Queue/QueuePanelContext';
-import { GradientHoverProvider } from './GradientHoverProvider';
-import { ScrollProvider } from './ScrollProvider';
-import SidebarProvider from './SideBarProvider';
-import { ThemeProvider } from './ThemeProvider';
-import { SlowedReverbProvider } from './SlowedReverbProvider';
-import { LayoutConfigProvider } from './LayoutConfigContext';
+"use client";
+
+import { ReactNode } from "react";
+import AIOverlayProvider from "../AI/AIOverlayContext";
+import LyricsOverlayProvider from "../Lyrics/LyricsOverlayContext";
+import { PlayerProvider } from "../Music/Player/usePlayer";
+import PanelProvider from "../Music/Queue/QueuePanelContext";
+import { GradientHoverProvider } from "./GradientHoverProvider";
+import { ScrollProvider } from "./ScrollProvider";
+import SidebarProvider from "./SideBarProvider";
+import { ThemeProvider } from "./ThemeProvider";
+import { SlowedReverbProvider } from "./SlowedReverbProvider";
+import { LayoutConfigProvider } from "./LayoutConfigContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAuthMiddleware } from "../Authentication/AuthMiddleware";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  queryClient.setDefaultOptions({
+    mutations: {
+      onMutate: createAuthMiddleware(queryClient),
+    },
+    queries: {
+      queryFn: createAuthMiddleware(queryClient),
+    },
+  });
   return (
-    <ThemeProvider defaultTheme='dark'>
-      <LayoutConfigProvider>
-        <GradientHoverProvider>
-          <ScrollProvider>
-            <SidebarProvider>
-              <SlowedReverbProvider>
-                <PlayerProvider>
-                  <PanelProvider>
-                    <LyricsOverlayProvider>
-                      <AIOverlayProvider>
-                        {children}
-                      </AIOverlayProvider>
-                    </LyricsOverlayProvider>
-                  </PanelProvider>
-                </PlayerProvider>
-              </SlowedReverbProvider>
-            </SidebarProvider>
-          </ScrollProvider>
-        </GradientHoverProvider>
-      </LayoutConfigProvider>
+    <ThemeProvider defaultTheme="dark">
+      <QueryClientProvider client={queryClient}>
+        <LayoutConfigProvider>
+          <GradientHoverProvider>
+            <ScrollProvider>
+              <SidebarProvider>
+                <SlowedReverbProvider>
+                  <PlayerProvider>
+                    <PanelProvider>
+                      <LyricsOverlayProvider>
+                        <AIOverlayProvider>{children}</AIOverlayProvider>
+                      </LyricsOverlayProvider>
+                    </PanelProvider>
+                  </PlayerProvider>
+                </SlowedReverbProvider>
+              </SidebarProvider>
+            </ScrollProvider>
+          </GradientHoverProvider>
+        </LayoutConfigProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
