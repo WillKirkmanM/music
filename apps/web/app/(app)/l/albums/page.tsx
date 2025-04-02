ff295825d05862ff21a8b33a3596b670a0223646
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-04-02 22:28:19 +0100

    Feat(l/albums/page): Dedicated Library Page with Albums Selected as Default

---
+++ apps/web/app/(app)/l/albums/page.tsx
@@ -0,0 +1,9 @@
+import LibraryButtons from "@/components/Layout/LibraryButtons";
+
+export default function AlbumsPage() {
+  return (
+    <div className="min-h-screen text-center">
+      <LibraryButtons initialSelectedField="Albums" />
+    </div>
+  );
+}
