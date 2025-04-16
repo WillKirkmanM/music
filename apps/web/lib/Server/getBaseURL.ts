c69f29ffab3916f6ae338fcffaded960b8768711
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-04-16 20:58:24 +0100

    Feat(getBaseURL.ts): Do Not Rely on server.local_address

--- apps/web/lib/Server/getBaseURL.ts
+++ apps/web/lib/Server/getBaseURL.ts
@@ -1,7 +1,19 @@
 export default function getBaseURL(): string {
-  if (typeof localStorage !== 'undefined') {
-    const server = JSON.parse(localStorage.getItem('server') || '{}');
-    return server.local_address || '';
+  if (typeof window !== 'undefined') {
+    if (typeof localStorage !== 'undefined') {
+      const serverConfig = localStorage.getItem('server_url');
+      if (serverConfig) {
+        return serverConfig;
+      }
+    }
+    
+    const url = new URL(window.location.href);
+    if (url.hostname === 'localhost' && url.port === '3000') {
+      return `${url.protocol}//${url.hostname}:3001`;
+    }
+    
+    return window.location.origin;
   }
+  
   return '';
 }
