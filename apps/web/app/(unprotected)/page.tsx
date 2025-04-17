959f760c41999405c1cf4b9d0a9a370a4d48f379
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-04-17 23:32:20 +0100

    Feat((unprotected)/page): Do Not Set local_address Initially

--- apps/web/app/(unprotected)/page.tsx
+++ apps/web/app/(unprotected)/page.tsx
@@ -4,7 +4,7 @@ import pl from "@/assets/pl-tp.png";
 import ServerSelectIcon from "@/components/Setup/Server/ServerSelectIcon";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ServerInfo } from "@music/sdk/types";
-import { Button } from '@music/ui/components/button';
+import { Button } from "@music/ui/components/button";
 import {
   Form,
   FormControl,
@@ -13,16 +13,19 @@ import {
   FormLabel,
   FormMessage,
 } from "@music/ui/components/form";
-import { Input } from '@music/ui/components/input';
+import { Input } from "@music/ui/components/input";
 import { Loader2Icon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as z from 'zod';
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
 
 const schema = z.object({
-  serverUrl: z.string().url({ message: 'Invalid URL' }).min(1, { message: 'Server URL is required' }),
+  serverUrl: z
+    .string()
+    .url({ message: "Invalid URL" })
+    .min(1, { message: "Server URL is required" }),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -39,8 +42,8 @@ export default function MainPage() {
 
       try {
         const storedServer = localStorage.getItem("server");
-        const serverUrl = storedServer 
-          ? (JSON.parse(storedServer).server_url || JSON.parse(storedServer).local_address)
+        const serverUrl = storedServer
+          ? JSON.parse(storedServer).server_url
           : localStorage.getItem("server_url") || window.location.origin;
 
         const response = await fetch(`${serverUrl}/api/s/server/info`);
@@ -75,37 +78,40 @@ export default function MainPage() {
   const form = useForm<FormData>({
     resolver: zodResolver(schema),
     defaultValues: {
-      serverUrl: window.location.origin
-    }
+      serverUrl: window.location.origin,
+    },
   });
 
   const { handleSubmit } = form;
 
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     setLoading(true);
-    
+
     try {
       const response = await fetch(`${data.serverUrl}/api/s/server/info`, {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        credentials: 'include'
+        credentials: "include",
       });
 
       const serverInfo = await response.json();
 
       localStorage.setItem("server_url", data.serverUrl);
-      
+
       if (serverInfo.product_name && serverInfo.startup_wizard_completed) {
         localStorage.setItem("server", JSON.stringify(serverInfo));
         push("/home");
       } else {
-        localStorage.setItem("server", JSON.stringify({ 
-          server_url: data.serverUrl,
-          server_name: serverInfo.server_name || "",
-          product_name: serverInfo.product_name || "ParsonLabs Music"
-        }));
-        
+        localStorage.setItem(
+          "server",
+          JSON.stringify({
+            server_url: data.serverUrl,
+            server_name: serverInfo.server_name || "",
+            product_name: serverInfo.product_name || "ParsonLabs Music",
+          })
+        );
+
         if (!serverInfo.startup_wizard_completed) {
           push("/setup/library");
         } else {
@@ -131,14 +137,19 @@ export default function MainPage() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-950 text-white">
         <Form {...form}>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 text-white w-80">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="space-y-6 text-white w-80"
+          >
             <p className="text-2xl mb-4">Enter Server URL</p>
             <FormField
               control={form.control}
               name="serverUrl"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="block text-sm font-medium text-white">Server URL</FormLabel>
+                  <FormLabel className="block text-sm font-medium text-white">
+                    Server URL
+                  </FormLabel>
                   <FormControl>
                     <Input
                       className="block w-full px-3 py-2 mt-1 border border-gray-600 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white bg-gray-800"
@@ -146,7 +157,9 @@ export default function MainPage() {
                       {...field}
                     />
                   </FormControl>
-                  <FormMessage className="text-sm text-red-600">{form.formState.errors.serverUrl?.message?.toString()}</FormMessage>
+                  <FormMessage className="text-sm text-red-600">
+                    {form.formState.errors.serverUrl?.message?.toString()}
+                  </FormMessage>
                 </FormItem>
               )}
             />
